27b7f256efb19efe921752ceb46fbdfbfdb555a3
Sebastian Troitzsch <s@troitzs.ch>
2019-12-10 13:49:47 +0800

    run_optimal_power_flow: Add constraints for voltage magnitude

--- examples/run_optimal_power_flow.jl
+++ examples/run_optimal_power_flow.jl
@@ -7,6 +7,7 @@ import GR
 import JuMP
 import Logging
 import Plots
+import Statistics
 import TimeSeries
 
 # Settings.
@@ -26,7 +27,20 @@ electric_grid_model = (
 linear_electric_grid_model = (
     FLEDGE.ElectricGridModels.LinearElectricGridModel(scenario_name)
 )
-Logging.@info("", linear_electric_grid_model)
+
+# Define derivative model parameters.
+load_active_power_vector_maximum = (
+    1.5 .* real.(electric_grid_model.load_power_vector_nominal)
+)
+load_active_power_vector_minimum = (
+    0.5 .* real.(electric_grid_model.load_power_vector_nominal)
+)
+load_reactive_power_vector_maximum = (
+    1.5 .* imag.(electric_grid_model.load_power_vector_nominal)
+)
+load_reactive_power_vector_minimum = (
+    0.5 .* imag.(electric_grid_model.load_power_vector_nominal)
+)
 
 # Instantiate optimization problem.
 optimization_problem = (
@@ -50,3 +64,105 @@ JuMP.@variable(
     optimization_problem,
     voltage_magnitude_per_unit_deviation_vector[electric_grid_index.nodes_phases]
 )
+
+# Define constraints.
+JuMP.@constraint(
+    optimization_problem,
+    load_active_minimum_maximum,
+    (
+        load_active_power_vector_minimum
+        .<=
+        load_active_power_vector.data
+        .<=
+        load_active_power_vector_maximum
+    )
+)
+JuMP.@constraint(
+    optimization_problem,
+    load_reactive_minimum_maximum,
+    (
+        load_reactive_power_vector_minimum
+        .<=
+        load_reactive_power_vector.data
+        .<=
+        load_reactive_power_vector_maximum
+    )
+)
+JuMP.@constraint(
+    optimization_problem,
+    voltage_magnitude_equation,
+    (
+        voltage_magnitude_vector.data
+        .==
+        (
+            linear_electric_grid_model.sensitivity_voltage_magnitude_by_power_wye_active
+            * electric_grid_model.load_incidence_wye_matrix
+            * load_active_power_vector.data
+            + linear_electric_grid_model.sensitivity_voltage_magnitude_by_power_wye_reactive
+            * electric_grid_model.load_incidence_wye_matrix
+            * load_reactive_power_vector.data
+            + linear_electric_grid_model.sensitivity_voltage_magnitude_by_power_delta_active
+            * electric_grid_model.load_incidence_delta_matrix
+            * load_active_power_vector.data
+            + linear_electric_grid_model.sensitivity_voltage_magnitude_by_power_delta_reactive
+            * electric_grid_model.load_incidence_delta_matrix
+            * load_reactive_power_vector.data
+        )
+    )
+)
+JuMP.@constraint(
+    optimization_problem,
+    voltage_magnitude_equation_0,
+    (
+        voltage_magnitude_per_unit_deviation_vector.data
+        .>=
+        0.0
+    )
+)
+JuMP.@constraint(
+    optimization_problem,
+    voltage_magnitude_equation_1,
+    (
+        voltage_magnitude_per_unit_deviation_vector.data
+        .>=
+        (
+            voltage_magnitude_vector.data
+            ./ abs.(electric_grid_model.nodal_voltage_vector_no_load)
+            .- 1.0
+        )
+    )
+)
+JuMP.@constraint(
+    optimization_problem,
+    voltage_magnitude_equation_2,
+    (
+        voltage_magnitude_per_unit_deviation_vector.data
+        .>=
+        -1.0
+        .* (
+            voltage_magnitude_vector.data
+            ./ abs.(electric_grid_model.nodal_voltage_vector_no_load)
+            .- 1.0
+        )
+    )
+)
+
+# Define objective.
+JuMP.@objective(
+    optimization_problem,
+    Min,
+    sum(voltage_magnitude_per_unit_deviation_vector.data)
+)
+
+# Solve optimization problem.
+Logging.@info("", optimization_problem)
+JuMP.optimize!(optimization_problem)
+
+# Get results.
+optimization_termination_status = JuMP.termination_status(optimization_problem)
+Logging.@info("", optimization_termination_status)
+load_active_power_vector_per_unit = (
+    JuMP.value.(load_active_power_vector.data)
+    ./ real.(electric_grid_model.load_power_vector_nominal)
+)
+Logging.@info("", Statistics.mean(load_active_power_vector_per_unit))

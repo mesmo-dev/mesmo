c504a5bef0c873a04dd8dbb89c14cc8634ab4f7b
Sebastian Troitzsch <s@troitzs.ch>
2020-01-09 10:58:14 +0800

    setup: Add Python setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""Setup script."""
+
+import setuptools
+
+setuptools.setup(
+    name='fledge',
+    version='0.3.0',
+    py_modules=setuptools.find_packages(),
+    install_requires=[
+        'hvplot',
+        'numpy',
+        'pandas',
+        'parameterized',  # For tests.
+        'pyomo',
+        'scipy',
+    ],
+)

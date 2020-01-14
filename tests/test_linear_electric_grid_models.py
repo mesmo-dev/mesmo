c45a0829a78ed9a7020e3f524303b2120b456b75
Sebastian Troitzsch <s@troitzs.ch>
2020-01-14 11:20:13 +0800

    test_linear_electric_grid_model: Cleanup detailed test

--- tests/test_linear_electric_grid_models.py
+++ tests/test_linear_electric_grid_models.py
@@ -6,8 +6,8 @@ import time
 import unittest
 
 import fledge.config
-import fledge.electric_grid_models
 import fledge.linear_electric_grid_models
+import fledge.electric_grid_models
 import fledge.power_flow_solvers
 
 
@@ -28,87 +28,83 @@ class TestLinearElectricGridModels(unittest.TestCase):
         electric_grid_model = fledge.electric_grid_models.ElectricGridModel(fledge.config.test_scenario_name)
 
         # Obtain power flow solution for nominal loading conditions.
-        power_flow_solution = fledge.power_flow_solvers.PowerFlowSolutionFixedPoint(electric_grid_model)
+        power_flow_solution_initial = fledge.power_flow_solvers.PowerFlowSolutionFixedPoint(electric_grid_model)
 
         # Obtain linear electric grid model for nominal loading conditions.
         linear_electric_grid_model = (
             fledge.linear_electric_grid_models.LinearElectricGridModel(
                 electric_grid_model,
-                power_flow_solution
+                power_flow_solution_initial
             )
         )
 
-        # Define power vector multipliers for testing of linear model at
-        # different loading conditions.
-        power_multipliers = np.arange(0.8, 1.25, 0.05)
-
         # Obtain nodal power vectors assuming nominal loading conditions.
-        # TODO: Rename to initial.
-        node_power_vector_wye = (
+        node_power_vector_wye_initial = (
             np.transpose([
                 electric_grid_model.load_incidence_wye_matrix
                 @ electric_grid_model.load_power_vector_nominal
             ])
         )
-        node_power_vector_delta = (
+        node_power_vector_delta_initial = (
             np.transpose([
                 electric_grid_model.load_incidence_delta_matrix
                 @ electric_grid_model.load_power_vector_nominal
             ])
         )
-        node_power_vector_wye_active = (
-            np.real(node_power_vector_wye)
+        node_power_vector_wye_active_initial = (
+            np.real(node_power_vector_wye_initial)
         )
-        node_power_vector_wye_reactive = (
-            np.imag(node_power_vector_wye)
+        node_power_vector_wye_reactive_initial = (
+            np.imag(node_power_vector_wye_initial)
         )
-        node_power_vector_delta_active = (
-            np.real(node_power_vector_delta)
+        node_power_vector_delta_active_initial = (
+            np.real(node_power_vector_delta_initial)
         )
-        node_power_vector_delta_reactive = (
-            np.imag(node_power_vector_delta)
+        node_power_vector_delta_reactive_initial = (
+            np.imag(node_power_vector_delta_initial)
         )
 
         # Obtain initial and no-load nodal voltage vectors.
-        node_voltage_vector_initial = power_flow_solution.node_voltage_vector
-        branch_power_vector_1_initial = power_flow_solution.branch_power_vector_1
-        branch_power_vector_2_initial = power_flow_solution.branch_power_vector_2
-        loss_initial = power_flow_solution.loss
+        node_voltage_vector_initial = power_flow_solution_initial.node_voltage_vector
+        branch_power_vector_1_initial = power_flow_solution_initial.branch_power_vector_1
+        branch_power_vector_2_initial = power_flow_solution_initial.branch_power_vector_2
+        loss_initial = power_flow_solution_initial.loss
 
-        node_voltage_vector_no_load = electric_grid_model.node_voltage_vector_no_load
+        # Define power vector multipliers for testing of linear model at different loading conditions.
+        power_multipliers = np.arange(0.8, 1.25, 0.05)
 
         # Instantiate testing arrays.
-        node_voltage_vector_magnitude_fixed_point = (
+        node_voltage_vector_magnitude_power_flow = (
             np.zeros((electric_grid_model.index.node_dimension, len(power_multipliers)), dtype=np.float)
         )
         node_voltage_vector_magnitude_linear_model = (
             np.zeros((electric_grid_model.index.node_dimension, len(power_multipliers)), dtype=np.float)
         )
-        node_voltage_vector_fixed_point = (
+        node_voltage_vector_power_flow = (
             np.zeros((electric_grid_model.index.node_dimension, len(power_multipliers)), dtype=np.complex)
         )
         node_voltage_vector_linear_model = (
             np.zeros((electric_grid_model.index.node_dimension, len(power_multipliers)), dtype=np.complex)
         )
-        branch_power_vector_1_squared_fixed_point = (
+        branch_power_vector_1_squared_power_flow = (
             np.zeros((electric_grid_model.index.branch_dimension, len(power_multipliers)), dtype=np.float)
         )
         branch_power_vector_1_squared_linear_model = (
             np.zeros((electric_grid_model.index.branch_dimension, len(power_multipliers)), dtype=np.float)
         )
-        branch_power_vector_2_squared_fixed_point = (
+        branch_power_vector_2_squared_power_flow = (
             np.zeros((electric_grid_model.index.branch_dimension, len(power_multipliers)), dtype=np.float)
         )
         branch_power_vector_2_squared_linear_model = (
             np.zeros((electric_grid_model.index.branch_dimension, len(power_multipliers)), dtype=np.float)
         )
-        loss_active_fixed_point = (
+        loss_active_power_flow = (
             np.zeros(len(power_multipliers), dtype=np.float)
         )
         loss_active_linear_model = (
             np.zeros(len(power_multipliers), dtype=np.float)
         )
-        loss_reactive_fixed_point = (
+        loss_reactive_power_flow = (
             np.zeros(len(power_multipliers), dtype=np.float)
         )
         loss_reactive_linear_model = (
@@ -142,68 +138,68 @@ class TestLinearElectricGridModels(unittest.TestCase):
             # Obtain nodal power vectors depending on multiplier.
             node_power_vector_wye_candidate = (
                 power_multiplier
-                * node_power_vector_wye
+                * node_power_vector_wye_initial
             )
             node_power_vector_delta_candidate = (
                 power_multiplier
-                * node_power_vector_delta
+                * node_power_vector_delta_initial
             )
-            node_power_vector_wye_candidate_active = (
+            node_power_vector_wye_active_candidate = (
                 np.real(node_power_vector_wye_candidate)
             )
-            node_power_vector_wye_candidate_reactive = (
+            node_power_vector_wye_reactive_candidate = (
                 np.imag(node_power_vector_wye_candidate)
             )
-            node_power_vector_delta_candidate_active = (
+            node_power_vector_delta_active_candidate = (
                 np.real(node_power_vector_delta_candidate)
             )
-            node_power_vector_delta_candidate_reactive = (
+            node_power_vector_delta_reactive_candidate = (
                 np.imag(node_power_vector_delta_candidate)
             )
 
             # Obtain fixed-point power flow solution.
-            power_flow_solution_fixed_point = (
+            power_flow_solution = (
                 fledge.power_flow_solvers.PowerFlowSolutionFixedPoint(
                     electric_grid_model,
                     node_power_vector_wye_candidate,
                     node_power_vector_delta_candidate
                 )
             )
-            node_voltage_vector_fixed_point[:, multiplier_index] = (
-                power_flow_solution_fixed_point.node_voltage_vector.flatten()
+            node_voltage_vector_power_flow[:, multiplier_index] = (
+                power_flow_solution.node_voltage_vector.flatten()
             )
-            node_voltage_vector_magnitude_fixed_point[:, multiplier_index] = (
-                abs(power_flow_solution_fixed_point.node_voltage_vector).flatten()
+            node_voltage_vector_magnitude_power_flow[:, multiplier_index] = (
+                abs(power_flow_solution.node_voltage_vector).flatten()
             )
-            branch_power_vector_1_squared_fixed_point[:, multiplier_index] = (
-                (abs(power_flow_solution_fixed_point.branch_power_vector_1) ** 2).flatten()
+            branch_power_vector_1_squared_power_flow[:, multiplier_index] = (
+                (abs(power_flow_solution.branch_power_vector_1) ** 2).flatten()
             )
-            branch_power_vector_2_squared_fixed_point[:, multiplier_index] = (
-                (abs(power_flow_solution_fixed_point.branch_power_vector_2) ** 2).flatten()
+            branch_power_vector_2_squared_power_flow[:, multiplier_index] = (
+                (abs(power_flow_solution.branch_power_vector_2) ** 2).flatten()
             )
-            loss_active_fixed_point[multiplier_index] = (
-                np.real([power_flow_solution_fixed_point.loss]).flatten()
+            loss_active_power_flow[multiplier_index] = (
+                np.real([power_flow_solution.loss]).flatten()
             )
-            loss_reactive_fixed_point[multiplier_index] = (
-                np.imag([power_flow_solution_fixed_point.loss]).flatten()
+            loss_reactive_power_flow[multiplier_index] = (
+                np.imag([power_flow_solution.loss]).flatten()
             )
 
             # Calculate nodal power difference / change.
             node_power_vector_wye_active_change = (
-                node_power_vector_wye_candidate_active
-                - node_power_vector_wye_active
+                node_power_vector_wye_active_candidate
+                - node_power_vector_wye_active_initial
             )
             node_power_vector_wye_reactive_change = (
-                node_power_vector_wye_candidate_reactive
-                - node_power_vector_wye_reactive
+                node_power_vector_wye_reactive_candidate
+                - node_power_vector_wye_reactive_initial
             )
             node_power_vector_delta_active_change = (
-                node_power_vector_delta_candidate_active
-                - node_power_vector_delta_active
+                node_power_vector_delta_active_candidate
+                - node_power_vector_delta_active_initial
             )
             node_power_vector_delta_reactive_change = (
-                node_power_vector_delta_candidate_reactive
-                - node_power_vector_delta_reactive
+                node_power_vector_delta_reactive_candidate
+                - node_power_vector_delta_reactive_initial
             )
 
             # Calculate approximate voltage, power vectors and total losses.
@@ -277,37 +273,37 @@ class TestLinearElectricGridModels(unittest.TestCase):
             # Calculate errors for voltage, power vectors and total losses.
             node_voltage_vector_error[multiplier_index] = (
                 get_error(
-                    node_voltage_vector_fixed_point[:, multiplier_index],
+                    node_voltage_vector_power_flow[:, multiplier_index],
                     node_voltage_vector_linear_model[:, multiplier_index]
                 )
             )
             node_voltage_vector_magnitude_error[multiplier_index] = (
                 get_error(
-                    node_voltage_vector_magnitude_fixed_point[:, multiplier_index],
+                    node_voltage_vector_magnitude_power_flow[:, multiplier_index],
                     node_voltage_vector_magnitude_linear_model[:, multiplier_index]
                 )
             )
             branch_power_vector_1_squared_error[multiplier_index] = (
                 get_error(
-                    branch_power_vector_1_squared_fixed_point[:, multiplier_index],
+                    branch_power_vector_1_squared_power_flow[:, multiplier_index],
                     branch_power_vector_1_squared_linear_model[:, multiplier_index]
                 )
             )
             branch_power_vector_2_squared_error[multiplier_index] = (
                 get_error(
-                    branch_power_vector_2_squared_fixed_point[:, multiplier_index],
+                    branch_power_vector_2_squared_power_flow[:, multiplier_index],
                     branch_power_vector_2_squared_linear_model[:, multiplier_index]
                 )
             )
             loss_active_error[multiplier_index] = (
                 get_error(
-                    loss_active_fixed_point[multiplier_index],
+                    loss_active_power_flow[multiplier_index],
                     loss_active_linear_model[multiplier_index]
                 )
             )
             loss_reactive_error[multiplier_index] = (
                 get_error(
-                    loss_reactive_fixed_point[multiplier_index],
+                    loss_reactive_power_flow[multiplier_index],
                     loss_reactive_linear_model[multiplier_index]
                 )
             )

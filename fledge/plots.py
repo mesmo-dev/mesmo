3fe49c2404d582c9535ad1382e804983c09449f3
Sebastian Troitzsch <s@troitzs.ch>
2020-07-01 22:08:36 +0800

    plots & test: Add thermal grid graph

--- fledge/plots.py
+++ fledge/plots.py
@@ -61,3 +61,50 @@ class ElectricGridGraph(nx.DiGraph):
         self.node_labels = (
             electric_grid_data.electric_grid_nodes.loc[:, 'node_name'].to_dict()
         )
+
+
+class ThermalGridGraph(nx.DiGraph):
+    """Electric grid graph object."""
+
+    node_positions: dict
+    node_labels: dict
+
+    @multimethod
+    def __init__(
+            self,
+            scenario_name: str
+    ):
+
+        # Obtain thermal grid data.
+        thermal_grid_data = fledge.data_interface.ThermalGridData(scenario_name)
+
+        self.__init__(
+            thermal_grid_data
+        )
+
+    @multimethod
+    def __init__(
+            self,
+            thermal_grid_data: fledge.data_interface.ThermalGridData
+    ):
+
+        # Create thermal grid graph.
+        super().__init__()
+        self.add_edges_from(
+            thermal_grid_data.thermal_grid_lines.loc[:, ['node_1_name', 'node_2_name']].itertuples(index=False)
+        )
+
+        # Remove nodes without latitude / longitude.
+        self.remove_nodes_from(
+            thermal_grid_data.thermal_grid_nodes.index[
+                thermal_grid_data.thermal_grid_nodes.loc[:, ['longitude', 'latitude']].isnull().any(axis='columns')
+            ]
+        )
+
+        # Obtain node positions / labels.
+        self.node_positions = (
+            thermal_grid_data.thermal_grid_nodes.loc[:, ['longitude', 'latitude']].T.to_dict('list')
+        )
+        self.node_labels = (
+            thermal_grid_data.thermal_grid_nodes.loc[:, 'node_name'].to_dict()
+        )
--- tests/test_plots.py
+++ tests/test_plots.py
@@ -17,3 +17,10 @@ class TestPlots(unittest.TestCase):
         fledge.plots.ElectricGridGraph(fledge.config.config['tests']['scenario_name'])
         time_duration = time.time() - time_start
         logger.info(f"Test ElectricGridGraph: Completed in {time_duration:.6f} seconds.")
+
+    def test_thermal_grid_graph(self):
+
+        time_start = time.time()
+        fledge.plots.ThermalGridGraph('singapore_tanjongpagar')
+        time_duration = time.time() - time_start
+        logger.info(f"Test ThermalGridGraph: Completed in {time_duration:.6f} seconds.")

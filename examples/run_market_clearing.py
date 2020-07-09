d7a0b784e6bd9ff3553855b03d9cdad66126b8d2
Christian Utama <christianutama@live.com>
2020-07-09 13:41:05 +0800

    examples: Minor fix to market clearing example

--- examples/run_market_clearing.py
+++ examples/run_market_clearing.py
@@ -32,7 +32,7 @@ def main():
 
     # TODO: Remove surrogate prices
     # Replace prices in the time series with surrogate prices
-    clearing_prices = pd.read_csv(os.path.join(price_data_path, 'Jan_2020.csv'), index_col=0)
+    clearing_prices = pd.read_csv(os.path.join(price_data_path, 'scenario_downtowncore.csv'), index_col=0)
     market_model.price_timeseries.loc[:, 'price_value'] = clearing_prices['clearing_price'].values / 1000
     flat_prices = pd.DataFrame(10.0, market_model.timesteps, ['price_value']) # For benchmarking
 
@@ -67,7 +67,7 @@ def main():
     for der_name in der_model_set.der_names:
         der_bids[der_name] = dict.fromkeys(timesteps)
         for timestep in timesteps:
-            der_bids[der_name][timestep] = pd.Series(0.0, index=range(0,5))
+            der_bids[der_name][timestep] = pd.Series(0.0, index=range(0,4))
 
     # Obtain benchmark costs (flat price)
     for der_name in der_model_set.der_names:
@@ -86,12 +86,13 @@ def main():
         baseline_dispatch[der_name] = output_vector['grid_electric_power']
 
     # Obtain bids from every building in the scenario
+    # TODO: formulate linear bid curves instead of price-pair quantities
     for timestep in timesteps:
         # Define price range and points for the current timestep
         lower_price_limit = price_forecast.at[timestep, 'lower_limit']
         upper_price_limit = price_forecast.at[timestep, 'upper_limit']
         price_points = np.linspace(lower_price_limit, upper_price_limit, 4)
-        price_points = np.append(price_points, [4.5])
+        # price_points = np.append(price_points, [4.5])
         for der_name in der_model_set.der_names:
             # Obtain DERModel from the model set
             der_model = der_model_set.flexible_der_models[der_name]
@@ -119,14 +120,16 @@ def main():
                     output_vector
                 ) = results['state_vector'], results['control_vector'], results['output_vector']
 
-                # Write output vector for debugging
-                # output_vector.to_csv(
-                #     os.path.join(results_path, f'output_vector_{timestep}_{der_name}_{price}.csv'.replace(':','_')))
-
                 der_bids[der_name][timestep][price] = output_vector.at[timestep, 'grid_electric_power']
 
             der_bids[der_name][timestep].iloc[:-1] -= der_bids[der_name][timestep].min() # Convert to block bids
             der_bids[der_name][timestep] = -der_bids[der_name][timestep] # Convert to negative power
+
+            # Save bids to CSV
+            der_bids[der_name][timestep].to_csv(os.path.join(
+                results_path, f'der_bids_{der_name}_{timestep}.csv'.replace(':','_')
+                )
+            )
 
         # Pass bids to MCE and obtain clearing price and power dispatch in return
         (
@@ -140,14 +143,14 @@ def main():
         for der_name in der_model_set.der_names:
             actual_dispatch[der_name][timestep] = -active_power_dispatch[der_name] # Convert back to positive to follow CoBMo convention
 
-        # TODO: Update forecast model with new market clearing price
-        if timestep == timesteps[-1]: # Skip forecast model update if at the last timestep
-            continue
-        new_timesteps = timesteps[timesteps > timestep]
-        forecast_timestep += dt.timedelta(minutes=30)
-        forecast_model.update_model(cleared_price, forecast_timestep)
-        price_forecast = forecast_model.forecast_prices(steps=len(new_timesteps))
-        price_forecast.index = new_timesteps
+        # Optional: update forecast with the new market clearing price
+        # if timestep == timesteps[-1]: # Skip forecast model update if at the last timestep
+        #     continue
+        # new_timesteps = timesteps[timesteps > timestep]
+        # forecast_timestep += dt.timedelta(minutes=30)
+        # forecast_model.update_model(cleared_price, forecast_timestep)
+        # price_forecast = forecast_model.forecast_prices(steps=len(new_timesteps))
+        # price_forecast.index = new_timesteps
 
     # Create dict to store electricity costs
     electricity_cost = dict.fromkeys(['baseline', 'bids'])
@@ -156,7 +159,7 @@ def main():
         for der_name in der_model_set.der_names:
             electricity_cost[key][der_name] = 0.0
 
-    print(actual_dispatch)
+
     # TODO: Calculate electricity costs for the buildings
     for der_name in der_model_set.der_names:
         electricity_cost['baseline'][der_name] = np.sum(

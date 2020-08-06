e1d1c0ba62824f1319538e303b672cbea15ce8ae
Sebastian Troitzsch <s@troitzs.ch>
2020-08-06 23:49:21 +0800

    run_electric_grid_model_power_validation: Better power flow validation for various load factors

--- examples/run_electric_grid_power_flow_validation.py
+++ examples/run_electric_grid_power_flow_validation.py
@@ -16,70 +16,184 @@ def main():
     # Settings.
     scenario_name = fledge.config.config['tests']['scenario_name']
     results_path = fledge.utils.get_results_path('run_electric_grid_power_flow_validation', scenario_name)
+    power_multipliers = np.arange(-0.2, 1.2, 0.1)
 
     # Recreate / overwrite database, to incorporate changes in the CSV files.
     fledge.data_interface.recreate_database()
 
-    # Obtain electric grid model.
+    # Obtain electric grid models.
     electric_grid_model_default = fledge.electric_grid_models.ElectricGridModelDefault(scenario_name)
     electric_grid_model_opendss = fledge.electric_grid_models.ElectricGridModelOpenDSS(scenario_name)
 
-    # Obtain power solution.
-    power_flow_solution_fixed_point = (
-        fledge.electric_grid_models.PowerFlowSolutionFixedPoint(electric_grid_model_default)
-    )
-    power_flow_solution_opendss = (
-        fledge.electric_grid_models.PowerFlowSolutionOpenDSS(electric_grid_model_opendss)
-    )
+    # Obtain nominal power flow solutions.
+    power_flow_solution_fledge_nominal = fledge.electric_grid_models.PowerFlowSolutionFixedPoint(electric_grid_model_default)
+    power_flow_solution_opendss_nominal = fledge.electric_grid_models.PowerFlowSolutionOpenDSS(electric_grid_model_opendss)
 
-    # Obtain results.
-    node_voltage_vector_magnitude_fixed_point = (
-        np.abs(power_flow_solution_fixed_point.node_voltage_vector)
+    # Instantiate results variables.
+    der_power_vector = (
+        pd.DataFrame(index=power_multipliers, columns=electric_grid_model_default.ders, dtype=np.float)
+    )
+    node_voltage_vector_fledge = (
+        pd.DataFrame(index=power_multipliers, columns=electric_grid_model_default.nodes, dtype=np.complex)
+    )
+    node_voltage_vector_opendss = (
+        pd.DataFrame(index=power_multipliers, columns=electric_grid_model_default.nodes, dtype=np.complex)
+    )
+    node_voltage_vector_magnitude_fledge = (
+        pd.DataFrame(index=power_multipliers, columns=electric_grid_model_default.nodes, dtype=np.float)
     )
     node_voltage_vector_magnitude_opendss = (
-        np.abs(power_flow_solution_opendss.node_voltage_vector)
+        pd.DataFrame(index=power_multipliers, columns=electric_grid_model_default.nodes, dtype=np.float)
     )
-    node_voltage_vector_magnitude_per_unit_fixed_point = (
-        node_voltage_vector_magnitude_fixed_point / np.abs(electric_grid_model_default.node_voltage_vector_reference)
-    )
-    node_voltage_vector_magnitude_per_unit_opendss = (
-        node_voltage_vector_magnitude_opendss / np.abs(electric_grid_model_default.node_voltage_vector_reference)
-    )
-    branch_power_vector_1_magnitude_fixed_point = (
-        np.abs(power_flow_solution_fixed_point.branch_power_vector_1)
+    branch_power_vector_1_magnitude_fledge = (
+        pd.DataFrame(index=power_multipliers, columns=electric_grid_model_default.branches, dtype=np.float)
     )
     branch_power_vector_1_magnitude_opendss = (
-        np.abs(power_flow_solution_opendss.branch_power_vector_1)
+        pd.DataFrame(index=power_multipliers, columns=electric_grid_model_default.branches, dtype=np.float)
     )
-    branch_power_vector_1_magnitude_per_unit_fixed_point = (
-        branch_power_vector_1_magnitude_fixed_point / electric_grid_model_default.branch_power_vector_magnitude_reference
-    )
-    branch_power_vector_1_magnitude_per_unit_opendss = (
-        branch_power_vector_1_magnitude_opendss / electric_grid_model_default.branch_power_vector_magnitude_reference
-    )
-    branch_power_vector_2_magnitude_fixed_point = (
-        np.abs(power_flow_solution_fixed_point.branch_power_vector_2)
+    branch_power_vector_2_magnitude_fledge = (
+        pd.DataFrame(index=power_multipliers, columns=electric_grid_model_default.branches, dtype=np.float)
     )
     branch_power_vector_2_magnitude_opendss = (
-        np.abs(power_flow_solution_opendss.branch_power_vector_2)
+        pd.DataFrame(index=power_multipliers, columns=electric_grid_model_default.branches, dtype=np.float)
     )
-    branch_power_vector_2_magnitude_per_unit_fixed_point = (
-        branch_power_vector_2_magnitude_fixed_point / electric_grid_model_default.branch_power_vector_magnitude_reference
+    loss_active_fledge = (
+        pd.Series(index=power_multipliers, dtype=np.float)
     )
-    branch_power_vector_2_magnitude_per_unit_opendss = (
-        branch_power_vector_2_magnitude_opendss / electric_grid_model_default.branch_power_vector_magnitude_reference
+    loss_active_opendss = (
+        pd.Series(index=power_multipliers, dtype=np.float)
+    )
+    loss_reactive_fledge = (
+        pd.Series(index=power_multipliers, dtype=np.float)
+    )
+    loss_reactive_opendss = (
+        pd.Series(index=power_multipliers, dtype=np.float)
     )
 
+    # Obtain DER power / change.
+    der_power_vector.loc[:, :] = (
+        np.transpose([power_multipliers])
+        @ np.array([np.real(electric_grid_model_default.der_power_vector_reference)])
+    )
+
+    # Obtain OpenDSS solutions.
+    power_flow_solutions_fledge = (
+        fledge.utils.starmap(
+            fledge.electric_grid_models.PowerFlowSolutionFixedPoint,
+            [(electric_grid_model_default, row) for row in der_power_vector.values]
+        )
+    )
+    power_flow_solutions_fledge = dict(zip(power_multipliers, power_flow_solutions_fledge))
+    for power_multiplier in power_multipliers:
+        power_flow_solution = power_flow_solutions_fledge[power_multiplier]
+        node_voltage_vector_fledge.loc[power_multiplier, :] = power_flow_solution.node_voltage_vector
+        node_voltage_vector_magnitude_fledge.loc[power_multiplier, :] = np.abs(power_flow_solution.node_voltage_vector)
+        branch_power_vector_1_magnitude_fledge.loc[power_multiplier, :] = np.abs(power_flow_solution.branch_power_vector_1)
+        branch_power_vector_2_magnitude_fledge.loc[power_multiplier, :] = np.abs(power_flow_solution.branch_power_vector_2)
+        loss_active_fledge.loc[power_multiplier] = np.real(power_flow_solution.loss)
+        loss_reactive_fledge.loc[power_multiplier] = np.imag(power_flow_solution.loss)
+
+    power_flow_solutions_opendss = (
+        fledge.utils.starmap(
+            fledge.electric_grid_models.PowerFlowSolutionOpenDSS,
+            [(electric_grid_model_opendss, row) for row in der_power_vector.values]
+        )
+    )
+    power_flow_solutions_opendss = dict(zip(power_multipliers, power_flow_solutions_opendss))
+    for power_multiplier in power_multipliers:
+        power_flow_solution = power_flow_solutions_opendss[power_multiplier]
+        node_voltage_vector_opendss.loc[power_multiplier, :] = power_flow_solution.node_voltage_vector
+        node_voltage_vector_magnitude_opendss.loc[power_multiplier, :] = np.abs(power_flow_solution.node_voltage_vector)
+        branch_power_vector_1_magnitude_opendss.loc[power_multiplier, :] = np.abs(power_flow_solution.branch_power_vector_1)
+        branch_power_vector_2_magnitude_opendss.loc[power_multiplier, :] = np.abs(power_flow_solution.branch_power_vector_2)
+        loss_active_opendss.loc[power_multiplier] = np.real(power_flow_solution.loss)
+        loss_reactive_opendss.loc[power_multiplier] = np.imag(power_flow_solution.loss)
+
+    # Obtain error values.
+    node_voltage_vector_error = (
+        100.0 * (
+            (node_voltage_vector_fledge - node_voltage_vector_opendss)
+            / node_voltage_vector_opendss
+        ).abs().mean(axis='columns')
+    )
+    node_voltage_vector_magnitude_error = (
+        100.0 * (
+            (node_voltage_vector_magnitude_fledge - node_voltage_vector_magnitude_opendss)
+            / node_voltage_vector_magnitude_opendss
+        ).mean(axis='columns')
+    )
+    branch_power_vector_1_magnitude_error = (
+        100.0 * (
+            (branch_power_vector_1_magnitude_fledge - branch_power_vector_1_magnitude_opendss)
+            / branch_power_vector_1_magnitude_opendss
+        ).mean(axis='columns')
+    )
+    branch_power_vector_2_magnitude_error = (
+        100.0 * (
+            (branch_power_vector_2_magnitude_fledge - branch_power_vector_2_magnitude_opendss)
+            / branch_power_vector_2_magnitude_opendss
+        ).mean(axis='columns')
+    )
+    loss_active_error = (
+        100.0 * (
+            (loss_active_fledge - loss_active_opendss)
+            / loss_active_opendss
+        )
+    )
+    loss_reactive_error = (
+        100.0 * (
+            (loss_reactive_fledge - loss_reactive_opendss)
+            / loss_reactive_opendss
+        )
+    )
+
+    # Obtain error table.
+    power_flow_solution_error = (
+        pd.DataFrame(
+            [
+                node_voltage_vector_error,
+                node_voltage_vector_magnitude_error,
+                branch_power_vector_1_magnitude_error,
+                branch_power_vector_2_magnitude_error,
+                loss_active_error,
+                loss_reactive_error
+            ],
+            index=[
+                'node_voltage_vector_error',
+                'node_voltage_vector_magnitude_error',
+                'branch_power_vector_1_magnitude_error',
+                'branch_power_vector_2_magnitude_error',
+                'loss_active_error',
+                'loss_reactive_error'
+            ]
+        )
+    )
+    power_flow_solution_error = power_flow_solution_error.round(2)
+
+    # Print results.
+    print(power_flow_solution_error)
+
+    # Store results as CSV.
+    power_flow_solution_error.to_csv(os.path.join(results_path, 'linear_electric_grid_model_error.csv'))
+
     # Plot results.
+
+    # Nominal OpenDSS solution comparison.
     plt.title('Node voltage magnitude [p.u.]')
     plt.scatter(
         range(len(electric_grid_model_default.nodes)),
-        node_voltage_vector_magnitude_per_unit_fixed_point,
+        (
+            np.abs(power_flow_solution_fledge_nominal.node_voltage_vector)
+            / np.abs(electric_grid_model_default.node_voltage_vector_reference)
+        ),
         marker=4
     )
     plt.scatter(
         range(len(electric_grid_model_default.nodes)),
-        node_voltage_vector_magnitude_per_unit_opendss,
+        (
+            np.abs(power_flow_solution_opendss_nominal.node_voltage_vector)
+            / np.abs(electric_grid_model_default.node_voltage_vector_reference)
+        ),
         marker=5
     )
     plt.xticks(
@@ -88,22 +202,28 @@ def main():
         rotation=45,
         ha='right'
     )
-    plt.legend(['Fixed point', 'OpenDSS'])
+    plt.legend(['FLEDGE', 'OpenDSS'])
     plt.grid()
     plt.tight_layout()
-    plt.savefig(os.path.join(results_path, f'{plt.gca().get_title()}.png'))
-    plt.show()
+    plt.savefig(os.path.join(results_path, 'nominal_voltage_magnitude.png'))
+    # plt.show()
     plt.close()
 
     plt.title('Branch power (direction 1) magnitude [p.u.]')
     plt.scatter(
         range(len(electric_grid_model_default.branches)),
-        branch_power_vector_1_magnitude_per_unit_fixed_point,
+        (
+            np.abs(power_flow_solution_fledge_nominal.branch_power_vector_1)
+            / electric_grid_model_default.branch_power_vector_magnitude_reference
+        ),
         marker=4
     )
     plt.scatter(
         range(len(electric_grid_model_default.branches)),
-        branch_power_vector_1_magnitude_per_unit_opendss,
+        (
+            np.abs(power_flow_solution_opendss_nominal.branch_power_vector_1)
+            / electric_grid_model_default.branch_power_vector_magnitude_reference
+        ),
         marker=5
     )
     plt.xticks(
@@ -112,22 +232,28 @@ def main():
         rotation=45,
         ha='right'
     )
-    plt.legend(['Fixed point', 'OpenDSS'])
+    plt.legend(['FLEDGE', 'OpenDSS'])
     plt.grid()
     plt.tight_layout()
-    plt.savefig(os.path.join(results_path, f'{plt.gca().get_title()}.png'))
-    plt.show()
+    plt.savefig(os.path.join(results_path, 'nominal_branch_power_1_magnitude.png'))
+    # plt.show()
     plt.close()
 
     plt.title('Branch power (direction 2) magnitude [p.u.]')
     plt.scatter(
         range(len(electric_grid_model_default.branches)),
-        branch_power_vector_2_magnitude_per_unit_fixed_point,
+        (
+            np.abs(power_flow_solution_fledge_nominal.branch_power_vector_2)
+            / electric_grid_model_default.branch_power_vector_magnitude_reference
+        ),
         marker=4
     )
     plt.scatter(
         range(len(electric_grid_model_default.branches)),
-        branch_power_vector_2_magnitude_per_unit_opendss,
+        (
+            np.abs(power_flow_solution_opendss_nominal.branch_power_vector_2)
+            / electric_grid_model_default.branch_power_vector_magnitude_reference
+        ),
         marker=5
     )
     plt.xticks(
@@ -136,27 +262,27 @@ def main():
         rotation=45,
         ha='right'
     )
-    plt.legend(['Fixed point', 'OpenDSS'])
+    plt.legend(['FLEDGE', 'OpenDSS'])
     plt.grid()
     plt.tight_layout()
-    plt.savefig(os.path.join(results_path, f'{plt.gca().get_title()}.png'))
-    plt.show()
+    plt.savefig(os.path.join(results_path, 'nominal_branch_power_2_magnitude.png'))
+    # plt.show()
     plt.close()
 
     plt.title('Total power loss [VA]')
     plt.scatter(
         [1, 2],
         [
-            np.real(power_flow_solution_fixed_point.loss),
-            np.imag(power_flow_solution_fixed_point.loss)
+            np.real(power_flow_solution_fledge_nominal.loss),
+            np.imag(power_flow_solution_fledge_nominal.loss)
         ],
         marker=4
     )
     plt.scatter(
         [1, 2],
         [
-            np.real(power_flow_solution_opendss.loss),
-            np.imag(power_flow_solution_opendss.loss)
+            np.real(power_flow_solution_opendss_nominal.loss),
+            np.imag(power_flow_solution_opendss_nominal.loss)
         ],
         marker=5
     )
@@ -166,11 +292,56 @@ def main():
         rotation=45,
         ha='right'
     )
-    plt.legend(['Fixed point', 'OpenDSS'])
+    plt.legend(['FLEDGE', 'OpenDSS'])
     plt.grid()
     plt.tight_layout()
-    plt.savefig(os.path.join(results_path, f'{plt.gca().get_title()}.png'))
-    plt.show()
+    plt.savefig(os.path.join(results_path, 'nominal_loss.png'))
+    # plt.show()
+    plt.close()
+
+    # Voltage magnitude.
+    for node_index, node in enumerate(electric_grid_model_default.nodes):
+        plt.plot(power_multipliers, node_voltage_vector_magnitude_opendss.loc[:, node], label='OpenDSS')
+        plt.plot(power_multipliers, node_voltage_vector_magnitude_fledge.loc[:, node], label='FLEDGE')
+        plt.legend()
+        plt.title(f"Voltage magnitude [V] for\n (node_type, node_name, phase): {node}")
+        plt.savefig(os.path.join(results_path, f'voltage_magnitude_{node}.png'))
+        # plt.show()
+        plt.close()
+
+    # Branch flow.
+    for branch_index, branch in enumerate(electric_grid_model_default.branches):
+        plt.plot(power_multipliers, branch_power_vector_1_magnitude_opendss.loc[:, branch], label='OpenDSS')
+        plt.plot(power_multipliers, branch_power_vector_1_magnitude_fledge.loc[:, branch], label='FLEDGE')
+        plt.legend()
+        plt.title(f"Branch power 1 magnitude [VA] for\n (branch_type, branch_name, phase): {branch}")
+        plt.savefig(os.path.join(results_path, f'branch_power_1_{branch}.png'))
+        # plt.show()
+        plt.close()
+
+        plt.plot(power_multipliers, branch_power_vector_2_magnitude_opendss.loc[:, branch], label='OpenDSS')
+        plt.plot(power_multipliers, branch_power_vector_2_magnitude_fledge.loc[:, branch], label='FLEDGE')
+        plt.legend()
+        plt.title(f"Branch power 2 magnitude [VA] for\n (branch_type, branch_name, phase): {branch}")
+        plt.savefig(os.path.join(results_path, f'branch_power_2_{branch}.png'))
+        # plt.show()
+        plt.close()
+
+    # Loss.
+    plt.plot(power_multipliers, loss_active_opendss, label='OpenDSS')
+    plt.plot(power_multipliers, loss_active_fledge, label='FLEDGE')
+    plt.legend()
+    plt.title("Total loss active [W]")
+    plt.savefig(os.path.join(results_path, f'loss_active.png'))
+    # plt.show()
+    plt.close()
+
+    plt.plot(power_multipliers, loss_reactive_opendss, label='OpenDSS')
+    plt.plot(power_multipliers, loss_reactive_fledge, label='FLEDGE')
+    plt.legend()
+    plt.title("Total loss reactive [VAr]")
+    plt.savefig(os.path.join(results_path, f'loss_reactive.png'))
+    # plt.show()
     plt.close()
 
     # Print results path.

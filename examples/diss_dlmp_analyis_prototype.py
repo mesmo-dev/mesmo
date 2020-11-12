950510af340561d58d00d5d1ff03b20196bd7ecd
Tom Schelo <tom.schelo@me.com>
2020-11-12 19:02:26 +0100

    examples: analysis script now implements plotting of branch flow, node voltage, der output and system losses

--- examples/diss_dlmp_analyis_prototype.py
+++ examples/diss_dlmp_analyis_prototype.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pyomo.environ as pyo
+import matplotlib.pyplot as plt
 
 import fledge.config
 import fledge.data_interface
@@ -20,15 +21,20 @@ path_to_solver_executable = '/Applications/CPLEX_Studio1210/cplex/bin/x86-64_osx
 
 def main():
 
-    # Generate the grids that are needed for different granularity levels
+    plots = True  # will generate plots if set to True
+    regenerate_grids = False  # will regenerate the grid input data
+
+    # Define grid / scenario names
     mv_grid_name = 'cigre_mv_network'
     high_granularity_scenario_name = 'cigre_high_granularity'
     low_granularity_scenario_name = 'cigre_low_granularity'
     no_granularity_scenario_name = low_granularity_scenario_name  # use same scenario, only without the actual grid
 
-    # path_to_grid_map = 'examples/electric_grid_mapping.csv'
-    # au.combine_electric_grids(mv_grid_name, path_to_grid_map, high_granularity_scenario_name)
-    # au.aggregate_electric_grids(mv_grid_name, path_to_grid_map, low_granularity_scenario_name)
+    # Generate the grids that are needed for different granularity levels (comment out if not needed)
+    if regenerate_grids:
+        path_to_grid_map = 'examples/electric_grid_mapping.csv'
+        au.combine_electric_grids(mv_grid_name, path_to_grid_map, high_granularity_scenario_name)
+        au.aggregate_electric_grids(mv_grid_name, path_to_grid_map, low_granularity_scenario_name)
 
     # Recreate / overwrite database, to incorporate the new grids that we created
     fledge.data_interface.recreate_database()
@@ -36,14 +42,14 @@ def main():
     # TODO: check if price_sensitivity_coefficient is being used and if it is set correctly?
     # Run centralized problems for all granularity levels
     scenarios = [high_granularity_scenario_name, low_granularity_scenario_name]
-    results_dict = {}
+    opf_results_dict = {}
     for scenario_name in scenarios:
         results_path = fledge.utils.get_results_path(
             'run_electric_grid_optimal_operation', scenario_name + '_central')
         opt_results, opf_results, dlmps = run_centralized_problem(scenario_name, results_path)
-        results_dict['opt_results_' + scenario_name + '_central'] = opt_results
-        results_dict['opf_results_' + scenario_name + '_central'] = opf_results
-        results_dict['dlmps_' + scenario_name] = dlmps
+        opf_results_dict['opt_results_' + scenario_name + '_central'] = opt_results
+        opf_results_dict['opf_results_' + scenario_name + '_central'] = opf_results
+        opf_results_dict['dlmps_' + scenario_name] = dlmps
         # results = run_nominal_operation(scenario_name, results_path)
 
     # Run decentralized problem based on the DLMPs and one based on wholesale market price (no granularity)
@@ -56,12 +62,12 @@ def main():
         # Obtain price data
         price_data = get_price_data_for_scenario(scenario_name)
         # Change price time series to dlmps
-        dlmps = results_dict['dlmps_' + scenario_name]
+        dlmps = opf_results_dict['dlmps_' + scenario_name]
         price_data.price_timeseries = dlmps['electric_grid_total_dlmp_price_timeseries']
         opt_results, opf_results = run_decentralized_problem(
             scenario_name, results_path, price_data)
-        results_dict['opt_results_' + scenario_name + '_decentral'] = opt_results
-        results_dict['opf_results_' + scenario_name + '_decentral'] = opf_results
+        opf_results_dict['opt_results_' + scenario_name + '_decentral'] = opt_results
+        opf_results_dict['opf_results_' + scenario_name + '_decentral'] = opf_results
         price_timeseries_dict[scenario_name] = price_data.price_timeseries
 
     # then the "no granularity" scenario based on the wholesale market
@@ -70,23 +76,33 @@ def main():
     results_path = fledge.utils.get_results_path('run_electric_grid_optimal_operation', scenario_name + '_decentral')
     opt_results, results = run_decentralized_problem(
         scenario_name, results_path, price_data)
-    results_dict['opt_results_' + 'cigre_no_granularity' + '_decentral'] = opt_results
-    results_dict['opf_results_' + 'cigre_no_granularity' + '_decentral'] = opf_results
-    price_timeseries_dict[scenario_name] = price_data.price_timeseries
+    opf_results_dict['opt_results_' + 'cigre_no_granularity' + '_decentral'] = opt_results
+    opf_results_dict['opf_results_' + 'cigre_no_granularity' + '_decentral'] = opf_results
+    price_timeseries_dict['cigre_no_granularity'] = price_data.price_timeseries
 
-    # Get the set points from decentralized problems and calculate power flow
+    # Get the set points from decentralized problems and calculate nominal power flow
     # now using the entire grid again
     pf_results_dict = {}
     scenario_name = high_granularity_scenario_name
-    for key in results_dict.keys():
+    for key in opf_results_dict.keys():
         if ('decentral' in key) and ('opf_results' in key):
             results_path = fledge.utils.get_results_path('run_electric_grid_nominal_operation', key + '_power_flow')
-            der_model_set = change_der_set_points_based_on_results(scenario_name, results_dict[key])
-            pf_results = run_nominal_operation(scenario_name, der_model_set, results_path)
-            pf_results_dict['pf_results_' + key] = pf_results
+            # change the set points of the DERs
+            der_model_set = change_der_set_points_based_on_results(scenario_name, opf_results_dict[key])
+            # run the nominal power flow and store results in dictionary
+            pf_results_dict['pf_results_' + key] = run_nominal_operation(scenario_name, der_model_set, results_path)
 
-    # TODO: plot results (and especially the differences) and price timeseries
-    # Ideas: ...
+    # Gather all result data in a dictionary and generate plots
+    if plots:
+        # Combine all results in one big dictionary
+        results_dict = {'opf_results': opf_results_dict,
+                        'pf_results': pf_results_dict,
+                        'price_timeseries': price_timeseries_dict}
+        # Pass to plot function
+        generate_result_plots(results_dict, high_granularity_scenario_name)
+
+    # TODO: repeat the above with higher DER penetration
+    # DERs can be modified based on the code example in function change_der_set_points...
 
     print('Done.')
 
@@ -310,6 +326,7 @@ def run_nominal_operation(
 def increase_der_penetration(
         electric_grid: fledge.data_interface.ElectricGridData
 ) -> fledge.data_interface.ElectricGridData:
+    # TODO: still to be implemented
     # some function to vary the DER penetration per iteration
     # what would be the input? the electric grid?
     pass
@@ -339,6 +356,124 @@ def get_price_data_for_scenario(
 ) -> fledge.data_interface.PriceData:
     scenario_data = fledge.data_interface.ScenarioData(scenario_name)
     return fledge.data_interface.PriceData(scenario_name, price_type=scenario_data.scenario['price_type'])
+
+
+def generate_result_plots(
+        results_dict: dict,
+        high_granularity_scenario_name: str,
+        results_path: str = None
+):
+    # This function produces the plots:
+    # [x] branch power magnitude,
+    # [x] node voltage magnitude
+    # [x] total losses
+    # [x] der dispatch
+    # [] objective result / system costs
+    # [] prices over time (see dlmp paper)
+    pf_results = results_dict['pf_results']
+    opf_results = results_dict['opf_results']
+    price_timeseries = results_dict['price_timeseries']
+
+    # Get the electric grid model of the high granularity (was the same for all power flow calculations)
+    electric_grid_model = fledge.electric_grid_models.ElectricGridModelDefault(
+        high_granularity_scenario_name)
+    scenario_data = fledge.data_interface.ScenarioData(high_granularity_scenario_name)  # only used for timesteps
+
+    # Branch power magnitude for both directions and node voltage
+    plots = {
+        'Branch power (direction 1) magnitude [p.u.]': 'branch_power_1_magnitude_per_unit',
+        'Branch power (direction 2) magnitude [p.u.]': 'branch_power_2_magnitude_per_unit',
+        'Node voltage magnitude [p.u.]': 'node_voltage_magnitude_per_unit',
+        'DER power magnitude [p.u.]': 'der_power_magnitude_per_unit',
+    }
+    for plot in plots.keys():
+        scenarios = get_list_of_scenario_strings(pf_results)
+        if 'Node' in plot:
+            x_index = electric_grid_model.nodes
+        elif 'Branch' in plot:
+            x_index = electric_grid_model.branches.append(electric_grid_model.transformers)
+        elif 'DER' in plot:
+            x_index = electric_grid_model.ders
+        for timestep in scenario_data.timesteps:
+            plt.title(plot)
+            marker_index = 4
+            for scenario_name in scenarios:
+                pf_results_scenario_name = [key for key in pf_results.keys() if scenario_name in key][0]
+                pf_results_of_scenario = pf_results[pf_results_scenario_name]
+                plt.scatter(
+                    range(len(x_index)),
+                    pf_results_of_scenario[plots[plot]].loc[timestep, :].reindex(x_index).values,
+                    marker=marker_index
+                )
+                marker_index += 1
+            plt.xticks(
+                range(len(x_index)),
+                x_index,
+                rotation=45,
+                ha='right'
+            )
+            plt.legend(scenarios)
+            plt.grid()
+            plt.tight_layout()
+            # plt.savefig(os.path.join(results_path, 'nominal_branch_power_1_magnitude.png'))
+            plt.show()
+            plt.close()
+
+    # TODO: generate objective result plot (system costs)
+    # Generate plots that are independent of branches / nodes:
+    plots = {
+        'System losses': 'loss_magnitude',
+        'Total system losses': 'loss_magnitude',
+    }
+    for plot in plots.keys():
+        scenarios = get_list_of_scenario_strings(pf_results)
+        if 'Total' in plot:
+            x_index = ['total']
+        else:
+            x_index = scenario_data.timesteps
+        plt.title(plot)
+        marker_index = 4
+        for scenario_name in scenarios:
+            pf_results_scenario_name = [key for key in pf_results.keys() if scenario_name in key][0]
+            pf_results_of_scenario = pf_results[pf_results_scenario_name]
+            if 'Total' in plot:
+                y_values = sum(pf_results_of_scenario[plots[plot]].values)
+            else:
+                y_values = pf_results_of_scenario[plots[plot]].values
+            plt.scatter(
+                range(len(x_index)),
+                y_values,
+                marker=marker_index
+            )
+            marker_index += 1
+        plt.xticks(
+            range(len(x_index)),
+            x_index,
+            rotation=45,
+            ha='right'
+        )
+        plt.legend(scenarios)
+        plt.grid()
+        plt.tight_layout()
+        # plt.savefig(os.path.join(results_path, 'nominal_branch_power_1_magnitude.png'))
+        plt.show()
+        plt.close()
+
+    if results_path is None:
+        # show plots - plt.show()
+        pass
+    else:
+        # save figures to path
+        pass
+
+
+def get_list_of_scenario_strings(
+        pf_results_dict: dict
+) -> list:
+    scenarios = []
+    for key in pf_results_dict.keys():
+        scenarios.append(key[len('pf_results_opf_results_'): -len('_decentral')])
+    return scenarios
 
 
 if __name__ == '__main__':

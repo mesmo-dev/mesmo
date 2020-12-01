00b112c9e907135e9e1daf1de0e18ecd105d64a7
Sebastian Troitzsch <s@troitzs.ch>
2020-12-01 20:59:33 +0800

    utils: Ensure that launch subprocess detaches & add path validity check

--- fledge/utils.py
+++ fledge/utils.py
@@ -283,12 +283,17 @@ def get_alphanumeric_string(
 def launch(path):
     """Launch the file at given path with its associated application. If path is a directory, open in file explorer."""
 
+    try:
+        assert os.path.exists(path)
+    except AssertionError:
+        logger.error(f'Cannot launch file or directory that does not exist: {path}')
+
     if sys.platform == 'win32':
         os.startfile(path)
     elif sys.platform == 'darwin':
-        subprocess.call(['open', path])
+        subprocess.Popen(['open', path], cwd="/", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     else:
-        subprocess.call(['xdg-open', path])
+        subprocess.Popen(['xdg-open', path], cwd="/", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 
 @fledge.config.memoize('get_building_model')

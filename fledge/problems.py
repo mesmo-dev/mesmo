16a32a74c7712fd35df5498ba8b0eaa08646aa98
Sebastian Troitzsch <s@troitzs.ch>
2020-12-14 21:42:18 +0800

    problems: Add results object

--- fledge/problems.py
+++ fledge/problems.py
@@ -446,3 +446,97 @@ class OptimalOperationProblem(object):
                 )
 
         return results
+
+
+class Results(object):
+    """Results object."""
+
+    # Models.
+    electric_grid_model: fledge.electric_grid_models.ElectricGridModel = None
+    thermal_grid_model: fledge.thermal_grid_models.ThermalGridModel = None
+    der_model_set: fledge.der_models.DERModelSet = None
+
+    # Price input data.
+    price_data: fledge.data_interface.PriceData = None
+
+    # Electric grid result variables.
+    der_power_vector: pd.DataFrame = None
+    node_voltage_vector: pd.DataFrame = None
+    branch_power_vector_1: pd.DataFrame = None
+    branch_power_vector_2: pd.DataFrame = None
+    loss: pd.DataFrame = None
+
+    # Thermal grid result variables.
+    der_thermal_power_vector: pd.DataFrame = None
+    node_head_vector: pd.DataFrame = None
+    branch_flow_vector: pd.DataFrame = None
+    pump_power: pd.DataFrame = None
+
+    # DER model result variables.
+    state_vector: pd.DataFrame = None
+    control_vector: pd.DataFrame = None
+    output_vector: pd.DataFrame = None
+    output_minimum_timeseries: pd.DataFrame = None
+    output_maximum_timeseries: pd.DataFrame = None
+
+    # Electric grid DLMPs.
+    electric_grid_energy_dlmp_node_active_power: pd.DataFrame = None
+    electric_grid_voltage_dlmp_node_active_power: pd.DataFrame = None
+    electric_grid_congestion_dlmp_node_active_power: pd.DataFrame = None
+    electric_grid_loss_dlmp_node_active_power: pd.DataFrame = None
+    electric_grid_total_dlmp_node_active_power: pd.DataFrame = None
+    electric_grid_voltage_dlmp_node_reactive_power: pd.DataFrame = None
+    electric_grid_congestion_dlmp_node_reactive_power: pd.DataFrame = None
+    electric_grid_loss_dlmp_node_reactive_power: pd.DataFrame = None
+    electric_grid_energy_dlmp_node_reactive_power: pd.DataFrame = None
+    electric_grid_total_dlmp_node_reactive_power: pd.DataFrame = None
+    electric_grid_energy_dlmp_der_active_power: pd.DataFrame = None
+    electric_grid_voltage_dlmp_der_active_power: pd.DataFrame = None
+    electric_grid_congestion_dlmp_der_active_power: pd.DataFrame = None
+    electric_grid_loss_dlmp_der_active_power: pd.DataFrame = None
+    electric_grid_total_dlmp_der_active_power: pd.DataFrame = None
+    electric_grid_voltage_dlmp_der_reactive_power: pd.DataFrame = None
+    electric_grid_congestion_dlmp_der_reactive_power: pd.DataFrame = None
+    electric_grid_loss_dlmp_der_reactive_power: pd.DataFrame = None
+    electric_grid_energy_dlmp_der_reactive_power: pd.DataFrame = None
+    electric_grid_total_dlmp_der_reactive_power: pd.DataFrame = None
+    electric_grid_total_dlmp_price_timeseries: pd.DataFrame = None
+
+    # Thermal grid DLMPs.
+    thermal_grid_energy_dlmp_node_thermal_power: pd.DataFrame = None
+    thermal_grid_head_dlmp_node_thermal_power: pd.DataFrame = None
+    thermal_grid_congestion_dlmp_node_thermal_power: pd.DataFrame = None
+    thermal_grid_pump_dlmp_node_thermal_power: pd.DataFrame = None
+    thermal_grid_total_dlmp_node_thermal_power: pd.DataFrame = None
+    thermal_grid_energy_dlmp_der_thermal_power: pd.DataFrame = None
+    thermal_grid_head_dlmp_der_thermal_power: pd.DataFrame = None
+    thermal_grid_congestion_dlmp_der_thermal_power: pd.DataFrame = None
+    thermal_grid_pump_dlmp_der_thermal_power: pd.DataFrame = None
+    thermal_grid_total_dlmp_der_thermal_power: pd.DataFrame = None
+    thermal_grid_total_dlmp_price_timeseries: pd.DataFrame = None
+
+    def __setattr__(self, attribute_name, value):
+
+        # Assert that attribute name is valid.
+        try:
+            assert hasattr(Results, attribute_name)
+        except AssertionError:
+            logger.error(f"Cannot set invalid results object variable: {attribute_name}")
+            raise
+
+        # Set attribute value.
+        super().__setattr__(attribute_name, value)
+
+    def __getattribute__(self, attribute_name):
+
+        # Obtain attribute value.
+        value = super().__getattribute__(attribute_name)
+
+        # Assert that attribute value has been set.
+        try:
+            assert value is not None
+        except AssertionError:
+            logger.error(f"Results variable '{attribute_name}' has no value / has not been set.")
+            raise
+
+        return value

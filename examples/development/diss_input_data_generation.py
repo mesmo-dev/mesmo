3629a44d468ad2482234981257aee35a64d31a9c
Tom Schelo <tom.schelo@me.com>
2020-12-21 11:03:49 +0100

    examples: added script for input data generation

---
+++ examples/development/diss_input_data_generation.py
@@ -0,0 +1,24 @@
+"""
+This script generates input data, e.g. load profiles for the scnenarios. It is only used once, which is why it is not
+part of the analysis script.
+"""
+
+import fledge.analysis_utils as au
+
+path_to_der_schedules_data = 'data/kerber_dorfnetz/der_schedules.csv'
+
+scenario_names = [
+    # 'NL_residential'
+    # 'kerber_landnetz_freileitung_1',
+    'kerber_landnetz_freileitung_2',
+    # 'kerber_landnetz_kabel_1',
+    # 'kerber_landnetz_kabel_2',
+    # 'kerber_dorfnetz',
+    # 'kerber_vorstadtnetz_kabel_1',
+    # 'kerber_vorstadtnetz_kabel_2',
+]
+
+au.generate_fixed_load_der_input_data(
+    scenario_names_list=scenario_names,
+    path_to_der_schedules_data=path_to_der_schedules_data
+)

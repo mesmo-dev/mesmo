bb93bc0b4b12e00f7e2e278834420d63d330ea8f
Sebastian Troitzsch <s@troitzs.ch>
2021-05-06 13:57:18 +0800

    examples: Add scripts for ISGT paper
    Co-authored-by: VKleins <69309521+VKleins@users.noreply.github.com>

---
+++ examples/run_MES_paper_ISGT.py
@@ -0,0 +1,519 @@
+"""Run script for reproducing results of the Paper XXX."""
+
+import matplotlib.dates
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import os
+import pandas as pd
+
+import fledge.config
+import fledge.data_interface
+import fledge.der_models
+import fledge.electric_grid_models
+import fledge.plots
+import fledge.problems
+import fledge.thermal_grid_models
+import fledge.utils
+
+
+def main(
+        scenario_number = 2
+):
+
+    # Settings.
+    scenario_number = 1 if scenario_number is None else scenario_number
+    # Choices:
+    # 1 - fixed loads and generators only,
+    # 2 - half of the loads flexible, all generators flexible,
+    # 3 - as in scenario 2 with additional conversion units
+    if scenario_number in [1]:
+        scenario_name = 'MES_scenario_1'
+    elif scenario_number in [2]:
+        scenario_name = 'MES_scenario_2'
+    elif scenario_number in [3]:
+        scenario_name = 'MES_scenario_3'
+
+    else:
+        scenario_name = 'MES_ISGT_scenario_3'
+
+    # Obtain results path.
+    results_path = fledge.utils.get_results_path(__file__, f'scenario{scenario_number}_{scenario_name}')
+
+    # Recreate / overwrite database, to incorporate changes in the CSV files.
+    fledge.data_interface.recreate_database()
+
+    # Obtain data.
+    scenario_data = fledge.data_interface.ScenarioData(scenario_name)
+    price_data = fledge.data_interface.PriceData(scenario_name)
+
+    # Obtain models.
+    electric_grid_model = fledge.electric_grid_models.ElectricGridModelDefault(scenario_name)
+    print(electric_grid_model)
+    power_flow_solution = fledge.electric_grid_models.PowerFlowSolutionFixedPoint(scenario_name)
+    linear_electric_grid_model = (
+        fledge.electric_grid_models.LinearElectricGridModelGlobal(
+            electric_grid_model,
+            power_flow_solution
+        )
+    )
+    thermal_grid_model = fledge.thermal_grid_models.ThermalGridModel(scenario_name)
+    print(thermal_grid_model)
+    thermal_power_flow_solution = fledge.thermal_grid_models.ThermalPowerFlowSolution(scenario_name)
+    linear_thermal_grid_model = (
+        fledge.thermal_grid_models.LinearThermalGridModel(
+            thermal_grid_model,
+            thermal_power_flow_solution
+        )
+    )
+    der_model_set = fledge.der_models.DERModelSet(scenario_name)
+
+    # Instantiate optimization problem.
+    optimization_problem = fledge.utils.OptimizationProblem()
+
+    # Define linear electric grid model variables.
+    linear_electric_grid_model.define_optimization_variables(
+        optimization_problem,
+        scenario_data.timesteps
+    )
+
+    # Define linear electric grid model constraints.
+    node_voltage_magnitude_vector_minimum = 0.5 * np.abs(electric_grid_model.node_voltage_vector_reference)
+    node_voltage_magnitude_vector_maximum = 1.5 * np.abs(electric_grid_model.node_voltage_vector_reference)
+    branch_power_magnitude_vector_maximum = 100.0 * electric_grid_model.branch_power_vector_magnitude_reference
+    # Modify limits for scenarios.
+
+    linear_electric_grid_model.define_optimization_constraints(
+        optimization_problem,
+        scenario_data.timesteps,
+        node_voltage_magnitude_vector_minimum=node_voltage_magnitude_vector_minimum,
+        node_voltage_magnitude_vector_maximum=node_voltage_magnitude_vector_maximum,
+        branch_power_magnitude_vector_maximum=branch_power_magnitude_vector_maximum
+    )
+
+    # Define thermal grid model variables.
+    linear_thermal_grid_model.define_optimization_variables(
+        optimization_problem,
+        scenario_data.timesteps
+    )
+
+    # Define thermal grid model constraints.
+
+    node_head_vector_minimum = 100.0 * thermal_power_flow_solution.node_head_vector
+    branch_flow_vector_maximum = 100.0 * np.abs(thermal_power_flow_solution.branch_flow_vector)
+
+    linear_thermal_grid_model.define_optimization_constraints(
+        optimization_problem,
+        scenario_data.timesteps,
+        node_head_vector_minimum=node_head_vector_minimum,
+        branch_flow_vector_maximum=branch_flow_vector_maximum
+    )
+
+    # Define DER variables.
+    der_model_set.define_optimization_variables(
+        optimization_problem
+    )
+
+    # Define DER constraints.
+    der_model_set.define_optimization_constraints(
+        optimization_problem,
+        electric_grid_model=electric_grid_model,
+        thermal_grid_model=thermal_grid_model
+    )
+
+    # Define electric grid objective.
+    linear_electric_grid_model.define_optimization_objective(
+        optimization_problem,
+        price_data,
+        scenario_data.timesteps
+    )
+
+    # Define thermal grid objective.
+    linear_thermal_grid_model.define_optimization_objective(
+        optimization_problem,
+        price_data,
+        scenario_data.timesteps
+    )
+
+    # Define DER objective.
+    der_model_set.define_optimization_objective(
+        optimization_problem,
+        price_data,
+        electric_grid_model=electric_grid_model,
+        thermal_grid_model=thermal_grid_model
+    )
+
+    # Solve optimization problem.
+    optimization_problem.solve()
+
+    # Obtain results.
+    results = fledge.problems.Results()
+    results.update(
+        linear_electric_grid_model.get_optimization_results(
+            optimization_problem,
+            power_flow_solution,
+            scenario_data.timesteps
+        )
+    )
+    results.update(
+        linear_thermal_grid_model.get_optimization_results(
+            optimization_problem,
+            scenario_data.timesteps
+        )
+    )
+    results.update(
+        der_model_set.get_optimization_results(
+            optimization_problem
+        )
+    )
+
+    # Print results.
+    print(results)
+
+    # Store results as CSV.
+    results.save(results_path)
+
+    # Obtain DLMPs.
+    dlmps = fledge.problems.Results()
+    dlmps.update(
+        linear_electric_grid_model.get_optimization_dlmps(
+            optimization_problem,
+            price_data,
+            scenario_data.timesteps
+        )
+    )
+    dlmps.update(
+        linear_thermal_grid_model.get_optimization_dlmps(
+            optimization_problem,
+            price_data,
+            scenario_data.timesteps
+        )
+    )
+
+    # Print DLMPs.
+    print(dlmps)
+
+    # Store DLMPs as CSV.
+    dlmps.save(results_path)
+
+    # Plot results.
+    in_per_unit = False
+    results_suffix = '_per_unit' if in_per_unit else ''
+
+    # Plot thermal grid DLMPs.
+    thermal_grid_dlmp = (
+        pd.concat(
+            [
+                dlmps['thermal_grid_energy_dlmp_node_thermal_power'],
+                dlmps['thermal_grid_pump_dlmp_node_thermal_power'],
+                dlmps['thermal_grid_head_dlmp_node_thermal_power'],
+                dlmps['thermal_grid_congestion_dlmp_node_thermal_power']
+            ],
+            axis='columns',
+            keys=['energy', 'pump', 'head', 'congestion'],
+            names=['dlmp_type']
+        )
+    )
+    colors = list(color['color'] for color in matplotlib.rcParams['axes.prop_cycle'])
+    for der in thermal_grid_model.ders:
+
+        # Obtain corresponding node.
+        node = (
+            thermal_grid_model.nodes[
+                thermal_grid_model.der_node_incidence_matrix[
+                    :,
+                    thermal_grid_model.ders.get_loc(der)
+                ].toarray().ravel() != 0
+            ]
+        )
+
+        # Create plot.
+        fig, (ax1, lax) = plt.subplots(ncols=2, figsize=[7.8, 2.6], gridspec_kw={"width_ratios": [100, 1]})
+        ax1.set_title(f"DER {der[1]} ({der[0].replace('_', ' ').capitalize()})")
+        ax1.stackplot(
+            scenario_data.timesteps,
+            (
+                thermal_grid_dlmp.loc[:, (slice(None), *zip(*node))].groupby('dlmp_type', axis='columns').mean().T
+                * 1.0e3
+            ),
+            labels=['Energy', 'Pumping', 'Head', 'Congest.'],
+            colors=[colors[0], colors[1], colors[2], colors[3]],
+            step='post'
+        )
+        ax1.plot(
+            (
+                thermal_grid_dlmp.loc[:, (slice(None), *zip(*node))].sum(axis='columns')
+                * 1.0e3
+            ),
+            label='Total DLMP',
+            drawstyle='steps-post',
+            color='red',
+            linewidth=1.0
+        )
+        ax1.grid(True)
+        ax1.set_xlabel('Time')
+        ax1.set_ylabel('Price [S$/MWh]')
+        # ax1.set_ylim((0.0, 10.0))
+        ax2 = plt.twinx(ax1)
+        if der in thermal_grid_model.ders:
+            ax2.plot(
+                results[f'der_thermal_power_vector{results_suffix}'].loc[:, der].abs() / (1 if in_per_unit else 1e6),
+                label='Thrm. pw.',
+                drawstyle='steps-post',
+                color='darkgrey',
+                linewidth=3
+            )
+        if der in electric_grid_model.ders:
+            ax2.plot(
+                results[f'der_active_power_vector{results_suffix}'].loc[:, der].abs() / (1 if in_per_unit else 1e6),
+                label='Active pw.',
+                drawstyle='steps-post',
+                color='black',
+                linewidth=1.5
+            )
+        ax2.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%H:%M'))
+        ax2.set_xlim((scenario_data.timesteps[0], scenario_data.timesteps[-1]))
+        ax2.set_xlabel('Time')
+        ax2.set_ylabel('Power [p.u.]') if in_per_unit else ax2.set_ylabel('Power [MW]')
+        # ax2.set_ylim((0.0, 1.0)) if in_per_unit else ax2.set_ylim((0.0, 30.0))
+        h1, l1 = ax1.get_legend_handles_labels()
+        h2, l2 = ax2.get_legend_handles_labels()
+        lax.legend((*h1, *h2), (*l1, *l2), borderaxespad=0)
+        lax.axis("off")
+        plt.tight_layout()
+        plt.savefig(os.path.join(results_path, f'thermal_grid_der_dlmp_{der}.png'))
+        # plt.show()
+        plt.close()
+
+    # Plot electric grid DLMPs.
+    electric_grid_dlmp = (
+        pd.concat(
+            [
+                dlmps['electric_grid_energy_dlmp_node_active_power'],
+                dlmps['electric_grid_loss_dlmp_node_active_power'],
+                dlmps['electric_grid_voltage_dlmp_node_active_power'],
+                dlmps['electric_grid_congestion_dlmp_node_active_power']
+            ],
+            axis='columns',
+            keys=['energy', 'loss', 'voltage', 'congestion'],
+            names=['dlmp_type']
+        )
+    )
+    colors = list(color['color'] for color in matplotlib.rcParams['axes.prop_cycle'])
+    for der in electric_grid_model.ders:
+
+        # Obtain corresponding node.
+        # TODO: Consider delta connected DERs.
+        node = (
+            electric_grid_model.nodes[
+                electric_grid_model.der_incidence_wye_matrix[
+                    :, electric_grid_model.ders.get_loc(der)
+                ].toarray().ravel() > 0
+            ]
+        )
+
+        # Create plot.
+        fig, (ax1, lax) = plt.subplots(ncols=2, figsize=[7.8, 2.6], gridspec_kw={"width_ratios": [100, 1]})
+        ax1.set_title(f"DER {der[1]} ({der[0].replace('_', ' ').capitalize()})")
+        ax1.stackplot(
+            scenario_data.timesteps,
+            (
+                electric_grid_dlmp.loc[:, (slice(None), *zip(*node))].groupby('dlmp_type', axis='columns').mean().T
+                * 1.0e3
+            ),
+            labels=['Energy', 'Loss', 'Voltage', 'Congest.'],
+            colors=[colors[0], colors[1], colors[2], colors[3]],
+            step='post'
+        )
+        ax1.plot(
+            (
+                electric_grid_dlmp.loc[
+                    :, (slice(None), *zip(*node))
+                ].groupby('dlmp_type', axis='columns').mean().sum(axis='columns')
+                * 1.0e3
+            ),
+            label='Total DLMP',
+            drawstyle='steps-post',
+            color='red',
+            linewidth=1.0
+        )
+        ax1.grid(True)
+        ax1.set_xlabel('Time')
+        ax1.set_ylabel('Price [S$/MWh]')
+        # ax1.set_ylim((0.0, 10.0))
+        ax2 = plt.twinx(ax1)
+        if der in thermal_grid_model.ders:
+            ax2.plot(
+                results[f'der_thermal_power_vector{results_suffix}'].loc[:, der].abs() / (1 if in_per_unit else 1e6),
+                label='Thrm. pw.',
+                drawstyle='steps-post',
+                color='darkgrey',
+                linewidth=3
+            )
+        if der in electric_grid_model.ders:
+            ax2.plot(
+                results[f'der_active_power_vector{results_suffix}'].loc[:, der].abs() / (1 if in_per_unit else 1e6),
+                label='Active pw.',
+                drawstyle='steps-post',
+                color='black',
+                linewidth=1.5
+            )
+        ax2.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%H:%M'))
+        ax2.set_xlim((scenario_data.timesteps[0], scenario_data.timesteps[-1]))
+        ax2.set_xlabel('Time')
+        ax2.set_ylabel('Power [p.u.]') if in_per_unit else ax2.set_ylabel('Power [MW]')
+        # ax2.set_ylim((0.0, 1.0)) if in_per_unit else ax2.set_ylim((0.0, 30.0))
+        h1, l1 = ax1.get_legend_handles_labels()
+        h2, l2 = ax2.get_legend_handles_labels()
+        lax.legend((*h1, *h2), (*l1, *l2), borderaxespad=0)
+        lax.axis("off")
+        plt.tight_layout()
+        plt.savefig(os.path.join(results_path, f'electric_grid_der_dlmp_{der}.png'))
+        # plt.show()
+        plt.close()
+
+    # Obtain graphs.
+    electric_grid_graph = fledge.plots.ElectricGridGraph(scenario_name)
+    thermal_grid_graph = fledge.plots.ThermalGridGraph(scenario_name)
+
+    # Plot thermal grid DLMPs in grid.
+    dlmp_types = [
+        'thermal_grid_energy_dlmp_node_thermal_power',
+        'thermal_grid_pump_dlmp_node_thermal_power',
+        'thermal_grid_head_dlmp_node_thermal_power',
+        'thermal_grid_congestion_dlmp_node_thermal_power'
+    ]
+    for timestep in scenario_data.timesteps:
+        for dlmp_type in dlmp_types:
+            node_color = (
+                dlmps[dlmp_type].loc[timestep, :].groupby('node_name').mean().reindex(thermal_grid_graph.nodes).values
+                * 1.0e3
+            )
+            plt.title(
+                f"{dlmp_type.replace('_', ' ').capitalize().replace('dlmp', 'DLMP')}"
+                f" at {timestep.strftime('%H:%M:%S')}"
+            )
+            nx.draw(
+                thermal_grid_graph,
+                pos=thermal_grid_graph.node_positions,
+                nodelist=(
+                    thermal_grid_model.nodes[
+                        fledge.utils.get_index(thermal_grid_model.nodes, node_type='source')
+                    ].get_level_values('node_name')[:1].to_list()
+                ),
+                edgelist=[],
+                node_size=150.0,
+                node_color='red'
+            )
+            nx.draw(
+                thermal_grid_graph,
+                pos=thermal_grid_graph.node_positions,
+                arrows=False,
+                node_size=100.0,
+                node_color=node_color,
+                edgecolors='black',  # Make node border visible.
+                with_labels=False
+            )
+            sm = (
+                plt.cm.ScalarMappable(
+                    norm=plt.Normalize(
+                        vmin=np.min(node_color),
+                        vmax=np.max(node_color)
+                    )
+                )
+            )
+            cb = plt.colorbar(sm, shrink=0.9)
+            cb.set_label('Price [S$/MWh]')
+            plt.tight_layout()
+            plt.savefig(os.path.join(results_path, f'{dlmp_type}_{timestep.strftime("%H-%M-%S")}.png'))
+            # plt.show()
+            plt.close()
+
+    # Plot electric grid DLMPs in grid.
+    dlmp_types = [
+        'electric_grid_energy_dlmp_node_active_power',
+        'electric_grid_voltage_dlmp_node_active_power',
+        'electric_grid_congestion_dlmp_node_active_power',
+        'electric_grid_loss_dlmp_node_active_power'
+    ]
+    for timestep in scenario_data.timesteps:
+        for dlmp_type in dlmp_types:
+            node_color = (
+                dlmps[dlmp_type].loc[timestep, :].groupby('node_name').mean().reindex(electric_grid_graph.nodes).values
+                * 1.0e3
+            )
+            plt.title(
+                f"{dlmp_type.replace('_', ' ').capitalize().replace('dlmp', 'DLMP')}"
+                f" at {timestep.strftime('%H:%M:%S')}"
+            )
+            nx.draw(
+                electric_grid_graph,
+                pos=electric_grid_graph.node_positions,
+                nodelist=(
+                    electric_grid_model.nodes[
+                        fledge.utils.get_index(electric_grid_model.nodes, node_type='source')
+                    ].get_level_values('node_name')[:1].to_list()
+                ),
+                edgelist=[],
+                node_size=150.0,
+                node_color='red'
+            )
+            nx.draw(
+                electric_grid_graph,
+                pos=electric_grid_graph.node_positions,
+                arrows=False,
+                node_size=100.0,
+                node_color=node_color,
+                edgecolors='black',  # Make node border visible.
+                with_labels=False
+            )
+            sm = (
+                plt.cm.ScalarMappable(
+                    norm=plt.Normalize(
+                        vmin=np.min(node_color),
+                        vmax=np.max(node_color)
+                    )
+                )
+            )
+            cb = plt.colorbar(sm, shrink=0.9)
+            cb.set_label('Price [S$/MWh]')
+            plt.tight_layout()
+            plt.savefig(os.path.join(results_path, f'{dlmp_type}_{timestep.strftime("%H-%M-%S")}.png'))
+            # plt.show()
+            plt.close()
+
+    # Plot electric grid line utilization.
+    fledge.plots.plot_grid_line_utilization(
+        electric_grid_model,
+        electric_grid_graph,
+        results[f'branch_power_magnitude_vector_1{results_suffix}'] * (100.0 if in_per_unit else 1.0e-3),
+        results_path,
+        value_unit='%' if in_per_unit else 'kW',
+    )
+    fledge.plots.plot_grid_line_utilization(
+        thermal_grid_model,
+        thermal_grid_graph,
+        results[f'branch_flow_vector{results_suffix}'] * (100.0 if in_per_unit else 1.0e-3),
+        results_path,
+        value_unit='%' if in_per_unit else 'kW',
+    )
+
+    # Plot electric grid nodes voltage drop.
+    fledge.plots.plot_grid_node_utilization(
+        electric_grid_model,
+        electric_grid_graph,
+        results['node_voltage_magnitude_vector_per_unit'],
+        results_path
+    )
+
+    # Print results path.
+    fledge.utils.launch(results_path)
+    print(f"Results are stored in: {results_path}")
+
+
+if __name__ == '__main__':
+
+    run_all = False
+
+    if run_all:
+        main()

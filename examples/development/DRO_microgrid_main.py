dro_mg_main: DRO works! some sign mistakes corrected

examples/development/DRO_microgrid_main.py
@@ -44,13 +44,13 @@ def main():
 
     # TODO: we need proper initialization of DRO data
     # constants
-    optimization_problem_dro.gamma = 300*np.ones((len(delta_indices_stage2), 1))
+    optimization_problem_dro.gamma = 30*np.ones((len(delta_indices_stage2), 1))
 
     optimization_problem_dro.delta_lower_bound = - 1 * np.ones((len(delta_indices_stage2), 1))
 
     optimization_problem_dro.delta_upper_bound = 1 * np.ones((len(delta_indices_stage2), 1))
 
-    optimization_problem_dro.u_upper_bound = 1000000 * np.ones((len(delta_indices_stage2), 1))
+    optimization_problem_dro.u_upper_bound = 1000 * np.ones((len(delta_indices_stage2), 1))
 
     # Define optimization problem variables
     optimization_problem_dro.s1_vector = cp.Variable((len(standard_form_stage_1.variables), 1))
@@ -124,6 +124,15 @@ def main():
     optimization_problem_dro.constraints.append(
         optimization_problem_dro.beta >= 0
     )
+
+    # optimization_problem_dro.constraints.append(
+    #     optimization_problem_dro.k_0_s2 == 0
+    # )
+    #
+    # optimization_problem_dro.constraints.append(
+    #     optimization_problem_dro.k_0_s3 == 0
+    # )
+
 
     print('define constr 35 (a-d)')
     # constr 35a)
@@ -2073,7 +2082,7 @@ def main():
                                 )
                             )
 
-    optimization_problem_dro.objective += (
+    optimization_problem_dro.objective -= (
         (
             f_vector.T
             @ optimization_problem_dro.s1_vector

examples/development/mg_offer_stage_1_problem_standard_form.py
@@ -483,9 +483,9 @@ def stage_1_problem_standard_form(scenario_name):
     )
 
     f_vector = np.zeros((len(standard_form.variables), 1))
-    f_vector[x_index_energy, 0] = np.array([price_timeseries_energy])
-    f_vector[x_index_up_reserve, 0] = -0.1 * np.array([price_timeseries_energy])
-    f_vector[x_index_down_reserve, 0] = -1.1 * np.array([price_timeseries_energy])
+    f_vector[x_index_energy, 0] = - np.array([price_timeseries_energy])
+    f_vector[x_index_up_reserve, 0] = 0.1 * np.array([price_timeseries_energy])
+    f_vector[x_index_down_reserve, 0] = 1.1 * np.array([price_timeseries_energy])
 
     # optimization_problem.objective += (
     #     (
@@ -530,7 +530,7 @@ def main():
     optimization_problem.constraints.append(
         a_matrix.toarray() @ optimization_problem.x_vector <= b_vector
     )
-    optimization_problem.objective += (
+    optimization_problem.objective -= (
             (
                 f_vector.T
                 @ optimization_problem.x_vector
511ddbc32bd80a46922d6d378ea3e70b35491a09
Tom Schelo <tom.schelo@me.com>
2021-07-12 15:10:50 +0200

    electric_grid_models: get_optimization_results()

--- examples/development/standard_form_example.py
+++ examples/development/standard_form_example.py
@@ -80,6 +80,7 @@ def main():
     optimization_problem.solve()
 
     # Obtain results.
+    results_grid = linear_electric_grid_model_set.get_optimization_results(optimization_problem)
     results_1 = optimization_problem.results
     duals_1 = optimization_problem.duals
     objective_1 = optimization_problem.evaluate_objective(optimization_problem.x_vector)

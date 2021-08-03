eb57d5d5abfe5b10b360464e587402aab8710271
Sebastian Troitzsch <s@troitzs.ch>
2021-08-03 16:42:23 +0800

    test: Run solve / get_result during problem tests (includes Gurobi check)

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -6,17 +6,31 @@ import fledge
 
 logger = fledge.config.get_logger(__name__)
 
+found_optimization_solver = False
+if fledge.config.config['optimization']['solver_name'] == 'gurobi':
+    import gurobipy
+    try:
+        gurobipy.Model()
+        found_optimization_solver = True
+    except gurobipy.GurobiError:
+        pass
+
 
 class TestProblems(unittest.TestCase):
 
     def test_nominal_operation_problem(self):
         # Get result.
         fledge.utils.log_time("test_nominal_operation_problem", log_level='info', logger_object=logger)
-        fledge.problems.NominalOperationProblem('singapore_tanjongpagar')
+        problem = fledge.problems.NominalOperationProblem('singapore_tanjongpagar')
+        problem.solve()
+        problem.get_results()
         fledge.utils.log_time("test_nominal_operation_problem", log_level='info', logger_object=logger)
 
     def test_optimal_operation_problem(self):
         # Get result.
         fledge.utils.log_time("test_optimal_operation_problem", log_level='info', logger_object=logger)
-        fledge.problems.OptimalOperationProblem('singapore_tanjongpagar')
+        problem = fledge.problems.OptimalOperationProblem('singapore_tanjongpagar')
+        if found_optimization_solver:
+            problem.solve()
+            problem.get_results()
         fledge.utils.log_time("test_optimal_operation_problem", log_level='info', logger_object=logger)

29f9b3e3cdfbb2580cccba626bf4cd8d3d3f224a
Sebastian Troitzsch <s@troitzs.ch>
2021-08-12 11:45:55 +0800

    electric_grid_models: Add todo

--- fledge/electric_grid_models.py
+++ fledge/electric_grid_models.py
@@ -4768,6 +4768,7 @@ class LinearElectricGridModelSet(object):
 
         # Instantiate DLMP variables.
         # TODO: Consider delta connections in nodal DLMPs.
+        # TODO: Consider single-phase DLMPs.
         electric_grid_energy_dlmp_node_active_power = (
             pd.DataFrame(columns=self.electric_grid_model.nodes, index=self.electric_grid_model.timesteps, dtype=float)
         )

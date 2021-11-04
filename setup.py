87293417895047acb4ecfbf7b6ee3033d3bb00c3
Sebastian Troitzsch <s@troitzs.ch>
2021-11-04 14:36:15 +0800

    setup: Fix / revise setup script

--- setup.py
+++ setup.py
@@ -1,30 +1,44 @@
 """Setup script."""
 
-import os
+import pathlib
 import setuptools
+import setuptools.command.develop
+import setuptools.command.install
 import subprocess
 import sys
 
 submodules = [
-    'cobmo'
+    'cobmo',
 ]
 
 # Check if submodules are loaded.
 for submodule in submodules:
-    if not os.path.exists(os.path.join(submodule, 'setup.py')):
+    if not (pathlib.Path(__file__).parent.absolute() / submodule / 'setup.py').is_file():
         raise FileNotFoundError(
-            f"No setup file found for submodule `{submodule}`. "
-            "Please check if the submodule is loaded correctly."
+            f"No setup file found for submodule `{submodule}`. Please check if the submodule is loaded correctly."
         )
 
-# Install submodules. Use `pip -v` to see subprocess outputs.
-for submodule in submodules:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-e', submodule])
+# Add post-installation routine to install submodules.
+class develop_submodules(setuptools.command.develop.develop):
+    def run(self):
+        super().run()
+        # Install submodules. Use `pip -v` to see subprocess outputs.
+        for submodule in submodules:
+            subprocess.check_call([sys.executable, '-m' 'pip', 'install', '-e', submodule])
+
+# Add post-installation routine to install submodules.
+class install_submodules(setuptools.command.install.install):
+    def run(self):
+        super().run()
+        # Install submodules. Use `pip -v` to see subprocess outputs.
+        for submodule in submodules:
+            subprocess.check_call([sys.executable, '-m' 'pip', 'install', '-e', submodule])
 
 setuptools.setup(
     name='mesmo',
     version='0.4.1',
     py_modules=setuptools.find_packages(),
+    cmdclass={'install': install_submodules, 'develop': develop_submodules},
     install_requires=[
         # Please note: Dependencies must also be added in `docs/conf.py` to `autodoc_mock_imports`.
         'cvxpy',

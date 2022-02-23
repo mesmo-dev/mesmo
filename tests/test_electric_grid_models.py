27a5a6ff08840e482898076aef74a25152f560df
Sebastian Troitzsch <s@troitzs.ch>
2022-02-23 14:29:21 +0800

    tests: Revise electric models tests

--- tests/test_electric_grid_models.py
+++ tests/test_electric_grid_models.py
@@ -1,5 +1,6 @@
 """Test electric grid models."""
 
+import inspect
 import unittest
 
 import mesmo
@@ -9,41 +10,36 @@ logger = mesmo.config.get_logger(__name__)
 
 class TestElectricGridModels(unittest.TestCase):
 
-    def test_electric_grid_model(self):
-        # Get result.
-        mesmo.utils.log_time("test_electric_grid_model", log_level='info', logger_object=logger)
-        mesmo.electric_grid_models.ElectricGridModel(mesmo.config.config['tests']['scenario_name'])
-        mesmo.utils.log_time("test_electric_grid_model", log_level='info', logger_object=logger)
+    def test_electric_grid_models(self):
 
-    def test_electric_grid_model_opendss(self):
-        # Get result.
-        mesmo.utils.log_time("test_electric_grid_model_opendss", log_level='info', logger_object=logger)
-        mesmo.electric_grid_models.ElectricGridModelOpenDSS(mesmo.config.config['tests']['scenario_name'])
-        mesmo.utils.log_time("test_electric_grid_model_opendss", log_level='info', logger_object=logger)
+        # Get module members.
+        for object_name, object_handle in inspect.getmembers(mesmo.electric_grid_models):
+            # Get classes, excluding base classes.
+            if inspect.isclass(object_handle) and not object_name.endswith('Base'):
+                # Exclude results classes.
+                if not issubclass(object_handle, mesmo.utils.ResultsBase):
 
-    def test_power_flow_solution_fixed_point(self):
-        # Get result.
-        mesmo.utils.log_time("test_power_flow_solution_fixed_point", log_level='info', logger_object=logger)
-        mesmo.electric_grid_models.PowerFlowSolutionFixedPoint(mesmo.config.config['tests']['scenario_name'])
-        mesmo.utils.log_time("test_power_flow_solution_fixed_point", log_level='info', logger_object=logger)
-
-    def test_power_flow_solution_z_bus(self):
-        # Get result.
-        mesmo.utils.log_time("test_power_flow_solution_z_bus", log_level='info', logger_object=logger)
-        mesmo.electric_grid_models.PowerFlowSolutionZBus(mesmo.config.config['tests']['scenario_name'])
-        mesmo.utils.log_time("test_power_flow_solution_z_bus", log_level='info', logger_object=logger)
-
-    def test_power_flow_solution_opendss(self):
-        # Get result.
-        mesmo.utils.log_time("test_power_flow_solution_opendss", log_level='info', logger_object=logger)
-        mesmo.electric_grid_models.PowerFlowSolutionOpenDSS(mesmo.config.config['tests']['scenario_name'])
-        mesmo.utils.log_time("test_power_flow_solution_opendss", log_level='info', logger_object=logger)
-
-    def test_linear_electric_grid_model_global(self):
-        # Get result.
-        mesmo.utils.log_time("test_linear_electric_grid_model_global", log_level='info', logger_object=logger)
-        mesmo.electric_grid_models.LinearElectricGridModelGlobal(mesmo.config.config['tests']['scenario_name'])
-        mesmo.utils.log_time("test_linear_electric_grid_model_global", log_level='info', logger_object=logger)
+                    # Test initialization routines.
+                    mesmo.utils.log_time(f"test `{object_name}`", log_level='info', logger_object=logger)
+                    if object_handle is mesmo.electric_grid_models.PowerFlowSolutionSet:
+                        # `PowerFlowSolutionSet` requires additional arguments.
+                        with self.subTest(object_name=object_name):
+                            electric_grid_model = mesmo.electric_grid_models.ElectricGridModel(
+                                mesmo.config.config['tests']['scenario_name']
+                            )
+                            der_model_set = mesmo.der_models.DERModelSet(
+                                mesmo.config.config['tests']['scenario_name']
+                            )
+                            der_operation_results = mesmo.electric_grid_models.ElectricGridDEROperationResults(
+                                der_active_power_vector=der_model_set.der_active_power_nominal_timeseries,
+                                der_reactive_power_vector=der_model_set.der_reactive_power_nominal_timeseries,
+                            )
+                            object_handle(electric_grid_model, der_operation_results)
+                    else:
+                        # Other object accept scenario name string.
+                        with self.subTest(object_name=object_name):
+                            object_handle(mesmo.config.config['tests']['scenario_name'])
+                    mesmo.utils.log_time(f"test `{object_name}`", log_level='info', logger_object=logger)
 
 
 if __name__ == '__main__':

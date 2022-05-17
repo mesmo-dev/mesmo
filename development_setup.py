05786eab8436b993c88a67d16feaeafb74eaec58
Sebastian Troitzsch <s@troitzs.ch>
2022-05-17 18:50:55 +0800

    setup & development_setup: Move development setup to dedicated script & retain clean setup script

---
+++ development_setup.py
@@ -0,0 +1,76 @@
+"""Development dependencies setup script."""
+
+import pathlib
+import requests
+import subprocess
+import sys
+import tarfile
+
+submodules = [
+    "cobmo",
+]
+
+# Get repository base path.
+base_path = pathlib.Path(__file__).parent.absolute()
+
+
+def main():
+
+    # Check if submodules are loaded.
+    print("Checking if submodules are loaded.")
+    for submodule in submodules:
+        if not (base_path / submodule / "setup.py").is_file():
+            try:
+                subprocess.check_call(["git", "-C", f"{base_path}", "submodule", "update", "--init", "--recursive"])
+            except FileNotFoundError:
+                raise FileNotFoundError(
+                    f"ERROR: No setup file found for submodule `{submodule}`. "
+                    f"Please check if the submodule is loaded correctly."
+                )
+
+    # Install submodules in develop mode. Use `pip -v` to see subprocess outputs.
+    print("Installing submodules in development mode.")
+    for submodule in submodules:
+        subprocess.check_call([sys.executable, "-m" "pip", "install", "-e", f"{base_path / submodule}"])
+
+    # Install MESMO.
+    print("Installing MESMO in development mode.")
+    subprocess.check_call([sys.executable, "-m" "pip", "install", "-e", f"{base_path}"])
+
+    # Install HiGHS solver.
+    print("Installing HiGHS solver.")
+    # Make HiGHS directory.
+    (base_path / "highs").mkdir(exist_ok=True)
+    # Construct HiGHS binary download URL.
+    base_url = "https://github.com/JuliaBinaryWrappers/HiGHSstatic_jll.jl/releases/download/"
+    version_string = "HiGHSstatic-v1.2.2%2B0/HiGHSstatic.v1.2.2"
+    if sys.platform == "win32":
+        architecture_string = "x86_64-w64-mingw32"
+    elif sys.platform == "darwin":
+        architecture_string = "x86_64-apple-darwin"
+    else:
+        architecture_string = "x86_64-linux-gnu-cxx11"
+    url = f"{base_url}{version_string}.{architecture_string}.tar.gz"
+    # Download and unpack HiGHS binary files.
+    try:
+        with requests.get(url, stream=True) as request:
+            request.raise_for_status()
+            with open(base_path / "highs" / "highs.tar.gz", "wb") as file:
+                for chunk in request.iter_content(chunk_size=10240):
+                    file.write(chunk)
+            with tarfile.open(base_path / "highs" / "highs.tar.gz") as file:
+                file.extractall(base_path / "highs")
+        # Remove downloaded archive file.
+        (base_path / "highs" / "highs.tar.gz").unlink()
+    except requests.ConnectionError:
+        # Soft-fail on download connection errors.
+        print(
+            "WARNING: HiGHS solver could not be installed automatically. "
+            "Please configure optimization solver for MESMO manually."
+        )
+    else:
+        print("Successfully installed HiGHS solver.")
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,91 +1,15 @@
 """Setup script."""
 
-import pathlib
-import requests
 import setuptools
-import setuptools.command.develop
-import setuptools.command.install
-import subprocess
-import sys
-import tarfile
-
-submodules = [
-    "cobmo",
-]
-
-# Check if submodules are loaded.
-base_path = pathlib.Path(__file__).parent.absolute()
-for submodule in submodules:
-    if not (base_path / submodule / "setup.py").is_file():
-        try:
-            subprocess.check_call(["git", "-C", str(base_path), "submodule", "update", "--init", "--recursive"])
-        except FileNotFoundError:
-            raise FileNotFoundError(
-                f"No setup file found for submodule `{submodule}`. Please check if the submodule is loaded correctly."
-            )
-
-# Add post-installation routine to install submodules in develop mode.
-class develop_submodules(setuptools.command.develop.develop):
-    def run(self):
-        super().run()
-        # Install submodules. Use `pip -v` to see subprocess outputs.
-        for submodule in submodules:
-            subprocess.check_call([sys.executable, "-m" "pip", "install", "-v", "-e", submodule])
-        # Install HiGHS.
-        install_highs()
-
-
-# Add post-installation routine to install submodules in normal mode.
-class install_submodules(setuptools.command.install.install):
-    def run(self):
-        super().run()
-        # Install submodules. Use `pip -v` to see subprocess outputs.
-        for submodule in submodules:
-            subprocess.check_call([sys.executable, "-m" "pip", "install", "-v", submodule])
-        # Install HiGHS solver.
-        install_highs()
-
-
-def install_highs():
-    # Make HiGHS directory.
-    (base_path / "highs").mkdir(exist_ok=True)
-    # Construct HiGHS binary download URL.
-    base_url = "https://github.com/JuliaBinaryWrappers/HiGHSstatic_jll.jl/releases/download/"
-    version_string = "HiGHSstatic-v1.2.2%2B0/HiGHSstatic.v1.2.2"
-    if sys.platform == "win32":
-        architecture_string = "x86_64-w64-mingw32"
-    elif sys.platform == "darwin":
-        architecture_string = "x86_64-apple-darwin"
-    else:
-        architecture_string = "x86_64-linux-gnu-cxx11"
-    url = f"{base_url}{version_string}.{architecture_string}.tar.gz"
-    # Download and unpack HiGHS binary files.
-    try:
-        with requests.get(url, stream=True) as request:
-            request.raise_for_status()
-            with open(base_path / "highs" / "highs.tar.gz", "wb") as file:
-                for chunk in request.iter_content(chunk_size=10240):
-                    file.write(chunk)
-            with tarfile.open(base_path / "highs" / "highs.tar.gz") as file:
-                file.extractall(base_path / "highs")
-        # Remove downloaded archive file.
-        (base_path / "highs" / "highs.tar.gz").unlink()
-    except requests.ConnectionError:
-        # Soft-fail on download connection errors.
-        print(
-            "WARNING: HiGHS solver could not be installed automatically. "
-            "Please configure optimization solver for MESMO manually."
-        )
-
 
 setuptools.setup(
     name="mesmo",
     version="0.5.0",
     py_modules=setuptools.find_packages(),
-    cmdclass={"install": install_submodules, "develop": develop_submodules},
     install_requires=[
         # Please note: Dependencies must also be added in `docs/conf.py` to `autodoc_mock_imports`.
         "cvxpy",
+        "cobmo",  # Not on package index. Needs to be manually installed from https://github.com/mesmo-dev/cobmo
         "dill",
         "dynaconf",
         "gurobipy",

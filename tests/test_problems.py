93f4464a66bb9ce9f9b0a9d5277dc45e0eeb2a0c
Sebastian Troitzsch <stroitzsch@users.noreply.github.com>
2022-06-10 12:16:10 +0000

    tests: Revise probblems module tests

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -1,25 +1,39 @@
 """Test problems."""
 
+import inspect
+from parameterized import parameterized
 import unittest
 
 import mesmo
 
 logger = mesmo.config.get_logger(__name__)
 
+# Get module members.
+objects = [
+    (object_name, object_handle)
+    for object_name, object_handle in inspect.getmembers(mesmo.problems)
+    # Get classes, excluding base classes.
+    if inspect.isclass(object_handle) and not object_name.endswith("Base")
+    # Exclude results classes.
+    and not issubclass(object_handle, mesmo.utils.ResultsBase)
+    # Exclude multimethod wrapper.
+    and not object_name == "multimethod"
+    # TODO: Exclude dict classes for now.
+    and not object_name.endswith("Dict")
+]
+
 
 class TestProblems(unittest.TestCase):
-    def test_nominal_operation_problem(self):
-        # Get result.
-        mesmo.utils.log_time("test_nominal_operation_problem", log_level="info", logger_object=logger)
-        problem = mesmo.problems.NominalOperationProblem("singapore_tanjongpagar")
-        problem.solve()
-        problem.get_results()
-        mesmo.utils.log_time("test_nominal_operation_problem", log_level="info", logger_object=logger)
+    @parameterized.expand(objects)
+    def test_problems(self, object_name, object_handle):
+        mesmo.utils.log_time(f"test `{object_name}`", log_level="info", logger_object=logger)
+        # Test initialization routines.
+        object_instance = object_handle(mesmo.config.config["tests"]["scenario_name"])
+        # Test solve(), get_results() routines.
+        object_instance.solve()
+        object_instance.get_results()
+        mesmo.utils.log_time(f"test `{object_name}`", log_level="info", logger_object=logger)
 
-    def test_optimal_operation_problem(self):
-        # Get result.
-        mesmo.utils.log_time("test_optimal_operation_problem", log_level="info", logger_object=logger)
-        problem = mesmo.problems.OptimalOperationProblem("singapore_tanjongpagar")
-        problem.solve()
-        problem.get_results()
-        mesmo.utils.log_time("test_optimal_operation_problem", log_level="info", logger_object=logger)
+
+if __name__ == "__main__":
+    unittest.main()

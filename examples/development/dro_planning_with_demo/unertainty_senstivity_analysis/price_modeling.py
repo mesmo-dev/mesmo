70b07081733e5689b87cc137d3fbbfe058c1ccf6
Kai Zhang <kaizhang@hotmail.de>
2022-08-25 11:01:45 +0800

    Initialize BSCS model

--- examples/development/dro_planning_with_demo/unertainty_senstivity_analysis/price_modeling.py
+++ examples/development/dro_planning_with_demo/unertainty_senstivity_analysis/price_modeling.py
@@ -1,9 +1,13 @@
 import mesmo
 import pandas as pd
 import plotly.express as px
+import numpy as np
+import os
+
 
 def main():
     # Settings.
+
     scenario_name = "uncertainty_sensitivity_analysis"
 
     # Settings.
@@ -13,11 +17,15 @@ def main():
     mesmo.data_interface.recreate_database()
     price_data = mesmo.data_interface.PriceData(scenario_name)
     wholesale_electricity_price_2017 = price_data.price_timeseries_raw
+    wholesale_electricity_price_2017.to_csv(str(os.getcwd())+'/electricity_price_data_2017')
 
     fig = px.line(wholesale_electricity_price_2017)
     fig.show()
     # TODO: neural network for price prediction
     #print("TensorFlow version:", tf.__version__)
+
+
+
 
 if __name__ == "__main__":
     main()
